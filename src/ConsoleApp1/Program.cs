8f9881206df5e8a80f0750a9e6d96ce2fc879c4e
Royer <stephane.royer@kpmg.lu>
2018-08-21 14:32:55 +0200

    rename folder

--- src/ConsoleApp1/Program.cs
+++ src/ConsoleApp1/Program.cs
@@ -17,48 +17,47 @@ namespace ConsoleApp1
 {
     class Program
     {
-        //static void Main(string[] args)
-        //{
-        //    //new StreamProcessRunner<TestJob3, MyConfig>().GetDefinitionStructure().OpenVisNetworkStructure();
-        //    var runner = new StreamProcessRunner<TestJob3, MyConfig>();
-        //    StreamProcessDefinition<TraceEvent> traceStreamProcessDefinition = null;// new StreamProcessDefinition<TraceEvent>(traceStream => traceStream.Where("keep log info", i => i.Content.Level <= TraceLevel.Info).ToAction("logs to console", Console.WriteLine));
-        //    var task = runner.ExecuteAsync(new MyConfig
-        //    {
-        //        InputFolderPath = @"C:\Users\paill\source\repos\Etl.Net\src\TestFiles\tmp\",
-        //        InputFilesSearchPattern = "*.txt",
-        //        TypeFilePath = @"C:\Users\paill\source\repos\Etl.Net\src\TestFiles\ref - Copy.txt",
-        //        DestinationFilePath = @"C:\Users\paill\source\repos\Etl.Net\src\TestFiles\outfile.csv"
-        //    }, traceStreamProcessDefinition);
-        //    task.Wait();
-
-        //    task.Result.OpenD3SankeyStatistics();
-
-        //    Console.WriteLine("Done");
-        //    Console.WriteLine("Press a key...");
-        //    Console.ReadKey();
-        //}
         static void Main(string[] args)
         {
-            //new StreamProcessRunner<TestJob3, MyConfig>().GetDefinitionStructure().OpenVisNetworkStructure();
-            var runner = new StreamProcessRunner<TestJob5, MyConfig2>();
-            StreamProcessDefinition<TraceEvent> traceStreamProcessDefinition = null;// new StreamProcessDefinition<TraceEvent>(traceStream => traceStream.Where("keep log info", i => i.Content.Level <= TraceLevel.Info).ToAction("logs to console", Console.WriteLine));
-            var task = runner.ExecuteAsync(new MyConfig2
-            {
-                ConnectionString = new System.Data.SqlClient.SqlConnectionStringBuilder
-                {
-                    DataSource = "localhost",
-                    InitialCatalog = "TestDB",
-                    IntegratedSecurity = true
-                }.ToString(),
-                Filter = "a"
-            }, traceStreamProcessDefinition);
-            task.Wait();
+           //new StreamProcessRunner<TestJob3, MyConfig>().GetDefinitionStructure().OpenVisNetworkStructure();
+           var runner = new StreamProcessRunner<TestJob3, MyConfig>();
+           StreamProcessDefinition<TraceEvent> traceStreamProcessDefinition = null;// new StreamProcessDefinition<TraceEvent>(traceStream => traceStream.Where("keep log info", i => i.Content.Level <= TraceLevel.Info).ToAction("logs to console", Console.WriteLine));
+           var task = runner.ExecuteAsync(new MyConfig
+           {
+               InputFolderPath = @"C:\Users\sroyer\Source\Repos\Etl.Net\src\TestFiles\",
+               InputFilesSearchPattern = "testin.*.txt",
+               TypeFilePath = @"C:\Users\sroyer\Source\Repos\Etl.Net\src\TestFiles\ref - Copy.txt",
+               DestinationFilePath = @"C:\Users\sroyer\Source\Repos\Etl.Net\src\TestFiles\outfile.csv"
+           }, traceStreamProcessDefinition);
+           task.Wait();
 
-            task.Result.OpenD3SankeyStatistics();
+           task.Result.OpenVisNetworkStatistics();
 
-            Console.WriteLine("Done");
-            Console.WriteLine("Press a key...");
-            Console.ReadKey();
+           Console.WriteLine("Done");
+           Console.WriteLine("Press a key...");
         }
+        // static void Main(string[] args)
+        // {
+        //     //new StreamProcessRunner<TestJob3, MyConfig>().GetDefinitionStructure().OpenVisNetworkStructure();
+        //     var runner = new StreamProcessRunner<TestJob5, MyConfig2>();
+        //     StreamProcessDefinition<TraceEvent> traceStreamProcessDefinition = null;// new StreamProcessDefinition<TraceEvent>(traceStream => traceStream.Where("keep log info", i => i.Content.Level <= TraceLevel.Info).ToAction("logs to console", Console.WriteLine));
+        //     var task = runner.ExecuteAsync(new MyConfig2
+        //     {
+        //         ConnectionString = new System.Data.SqlClient.SqlConnectionStringBuilder
+        //         {
+        //             DataSource = "localhost",
+        //             InitialCatalog = "TestDB",
+        //             IntegratedSecurity = true
+        //         }.ToString(),
+        //         Filter = "a"
+        //     }, traceStreamProcessDefinition);
+        //     task.Wait();
+
+        //     task.Result.OpenD3SankeyStatistics();
+
+        //     Console.WriteLine("Done");
+        //     Console.WriteLine("Press a key...");
+        //     Console.ReadKey();
+        // }
     }
 }
