c3a9e81acb66b48b578a1630a4040e8753e8df0c
Stéphane Royer <paillave@live.com>
2018-08-23 01:36:34 +0200

    renaming

--- src/Paillave.Etl.EntityFrameworkCore/ValuesProviders/EntityFrameworkCoreValueProvider.cs
+++ src/Paillave.Etl.EntityFrameworkCore/ValuesProviders/EntityFrameworkCoreValueProvider.cs
@@ -8,24 +8,24 @@ using System.Text;
 
 namespace Paillave.Etl.EntityFrameworkCore.ValuesProviders
 {
-    public class EntityFrameworkCoreValueProviderArgs<TIn, TRes, TOut>
+    public class EntityFrameworkCoreValueProviderArgs<TIn, TContext, TOut>
     {
+        public Func<TIn, TContext, IQueryable<TOut>> GetQuery { get; set; }
         public bool NoParallelisation { get; set; } = false;
-        public Func<TIn, TRes, IQueryable<TOut>> GetQuery { get; set; }
     }
-    public class EntityFrameworkCoreValueProvider<TIn, TRes, TOut> : ValuesProviderBase<TIn, TRes, TOut> where TRes : DbContext
+    public class EntityFrameworkCoreValueProvider<TIn, TContext, TOut> : ValuesProviderBase<TIn, TContext, TOut> where TContext : DbContext
     {
-        private EntityFrameworkCoreValueProviderArgs<TIn, TRes, TOut> _args;
-        public EntityFrameworkCoreValueProvider(EntityFrameworkCoreValueProviderArgs<TIn, TRes, TOut> args) : base(args.NoParallelisation)
+        private EntityFrameworkCoreValueProviderArgs<TIn, TContext, TOut> _args;
+        public EntityFrameworkCoreValueProvider(EntityFrameworkCoreValueProviderArgs<TIn, TContext, TOut> args) : base(args.NoParallelisation)
         {
             _args = args;
         }
-        public override IDeferedPushObservable<TOut> PushValues(TRes resource, TIn input)
+        public override IDeferedPushObservable<TOut> PushValues(TContext context, TIn input)
         {
             return new DeferedPushObservable<TOut>(pushValue =>
             {
                 WaitOne();
-                foreach (var item in _args.GetQuery(input, resource).ToList())
+                foreach (var item in _args.GetQuery(input, context).ToList())
                     pushValue(item);
                 Release();
             });
