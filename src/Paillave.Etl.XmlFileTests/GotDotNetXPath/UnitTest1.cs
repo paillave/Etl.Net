1e435da4436da9c3639ab2c393559d29bca5aa5f
Royer <stephane.royer@kpmg.lu>
2018-09-12 19:37:44 +0200

    test for xml

---
+++ src/Paillave.Etl.XmlFileTests/GotDotNetXPath/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Paillave.Etl.XmlFileTests
+{
+    // [TestClass]
+    // public class UnitTest1
+    // {
+    //     [TestMethod]
+    //     public void TestMethod1()
+    //     {
+    //     }
+    // }
+}
