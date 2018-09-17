f0f7d5952a6ae1bc5545709b844727f019172c19
Royer <stephane.royer@kpmg.lu>
2018-09-17 19:22:35 +0200

    start test sub process

---
+++ src/Samples/SubProcessQuickStart/Jobs/SubProcessQuickstartJob.cs
@@ -0,0 +1,50 @@
+﻿using SubProcessQuickStart.StreamTypes;
+using System.IO;
+using Paillave.Etl;
+using Paillave.Etl.Core.Streams;
+using System;
+
+namespace SubProcessQuickStart.Jobs
+{
+    public class SubProcessQuickstartJob : IStreamProcessDefinition<MyConfig>
+    {
+        public string Name => "import file";
+
+        public void DefineProcess(IStream<MyConfig> rootStream)
+        {
+            var outputFileResourceS = rootStream.Select("open output file", i => File.OpenWrite(i.DestinationFilePath));
+
+            var parsedLineS = rootStream
+                .CrossApplyFolderFiles("get folder files", i => i.InputFolderPath, i => i.InputFilesSearchPattern)
+                .CrossApplyTextFile("parse input file", new InputFileRowMapper(), (i, p) => { p.FileName = i; return p; });
+
+            var parsedTypeLineS = rootStream
+                .Select("get input file type path", i => i.TypeFilePath)
+                .CrossApplyTextFile("parse type input file", new TypeFileRowMapper());
+
+            var categoryS = parsedTypeLineS.Distinct("get list of categories", i => i.Category);
+
+            var joinedLineS = parsedLineS
+                .Lookup("join types to file", parsedTypeLineS, i => i.TypeId, i => i.Id, (l, r) => new { l.Id, r.Name, l.FileName, r.Category });
+
+            categoryS.SubExecute("export data per category", singleCategoryS =>
+            {
+                var subProcessLines = joinedLineS
+                    .Select("link to subprocess", singleCategoryS, (l, r) => new { Category = r.Category, Line = l })
+                    .Where("keep only line for current sub process", i => i.Category == i.Line.Category)
+                    .Select("keep line only", i => i.Line);
+
+                var outputCategoryResourceS = singleCategoryS.Select("open output category file", rootStream, (i, j) => File.OpenWrite(Path.Combine(j.CategoryDestinationFolder, "Category-" + i.Category) + ".csv"));
+
+                return subProcessLines
+                    .Pivot("create statistic for categories", i => i.Category, i => new { Count = AggregationOperators.Count(), Total = AggregationOperators.Sum(i.Id) })
+                    .Select("create output category data", i => new OutputCategoryRow { Category = i.Key, AmountOfEntries = i.Aggregation.Count, TotalAmount = i.Aggregation.Total })
+                    .ToTextFile("write category statistics to file", outputCategoryResourceS, new OutputCategoryRowMapper());
+            });
+
+            joinedLineS.Select("create output data", i => new OutputFileRow { Id = i.Id, Name = i.Name, FileName = i.FileName })
+                .ToTextFile("write to output file", outputFileResourceS, new OutputFileRowMapper())
+                .ToAction("write to console", i => Console.WriteLine($"{i.FileName}:{i.Id}-{i.Name}"));
+        }
+    }
+}
---
+++ src/Samples/SubProcessQuickStart/StreamTypes/InputFileRow.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Globalization;
+using Paillave.Etl.TextFile.Core;
+
+namespace SubProcessQuickStart.StreamTypes
+{
+    public class InputFileRow
+    {
+        public int Id { get; set; }
+        public DateTime Col1 { get; set; }
+        public decimal Col2 { get; set; }
+        public int Col3 { get; set; }
+        public string Col4 { get; set; }
+        public int TypeId { get; set; }
+        public string FileName { get; set; }
+    }
+
+    public class InputFileRowMapper : FlatFileDefinition<InputFileRow>
+    {
+        public InputFileRowMapper()
+        {
+            CultureInfo ci = CultureInfo.CreateSpecificCulture("en-GB");
+            ci.DateTimeFormat.FullDateTimePattern = "yyyy-MM-dd HH:mm:ss";
+            ci.DateTimeFormat.LongDatePattern = "yyyy-MM-dd";
+            ci.DateTimeFormat.ShortDatePattern = "yyyy-MM-dd";
+
+            ci.DateTimeFormat.FullDateTimePattern = "yyyy-MM-dd HH:mm:ss";
+            ci.DateTimeFormat.LongDatePattern = "yyyy-MM-dd";
+            ci.DateTimeFormat.ShortDatePattern = "yyyy-MM-dd";
+
+            ci.NumberFormat.NumberDecimalSeparator = ",";
+            ci.NumberFormat.CurrencyDecimalSeparator = ",";
+            ci.NumberFormat.PercentDecimalSeparator = ",";
+
+            this.WithCultureInfo(ci);
+            this.MapColumnToProperty("#", i => i.Id);
+            this.MapColumnToProperty("DateTime", i => i.Col1);
+            this.MapColumnToProperty("Value", i => i.Col2);
+            this.MapColumnToProperty("Rank", i => i.Col3);
+            this.MapColumnToProperty("Comment", i => i.Col4);
+            this.MapColumnToProperty("TypeId", i => i.TypeId);
+            this.IsColumnSeparated('\t');
+        }
+    }
+}
