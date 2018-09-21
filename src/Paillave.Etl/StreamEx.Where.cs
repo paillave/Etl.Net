4ecbace668a6816e85d9c35f4a23ed760f29e3f8
Royer <stephane.royer@kpmg.lu>
2018-09-21 14:09:59 +0200

    dispatch node helpers & add context to script task

---
+++ src/Paillave.Etl/StreamEx.Where.cs
@@ -0,0 +1,46 @@
+using Paillave.Etl.Core;
+using Paillave.Etl.StreamNodes;
+using Paillave.Etl.Core.Streams;
+using Paillave.Etl.Core.TraceContents;
+using Paillave.Etl.ValuesProviders;
+using Paillave.Etl.Reactive.Core;
+using Paillave.Etl.Reactive.Operators;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using SystemIO = System.IO;
+
+namespace Paillave.Etl
+{
+    public static partial class StreamEx
+    {
+        public static IKeyedStream<TIn, TKey> Where<TIn, TKey>(this IKeyedStream<TIn, TKey> stream, string name, Func<TIn, bool> predicate)
+        {
+            return new WhereStreamNode<TIn, IKeyedStream<TIn, TKey>>(name, new WhereArgs<TIn, IKeyedStream<TIn, TKey>>
+            {
+                Input = stream,
+                Predicate = predicate
+            }).Output;
+        }
+        public static ISortedStream<TIn, TKey> Where<TIn, TKey>(this ISortedStream<TIn, TKey> stream, string name, Func<TIn, bool> predicate)
+        {
+            return new WhereStreamNode<TIn, ISortedStream<TIn, TKey>>(name, new WhereArgs<TIn, ISortedStream<TIn, TKey>>
+            {
+                Input = stream,
+                Predicate = predicate
+            }).Output;
+        }
+        public static IStream<TIn> Where<TIn>(this IStream<TIn> stream, string name, Func<TIn, bool> predicate)
+        {
+            return new WhereStreamNode<TIn, IStream<TIn>>(name, new WhereArgs<TIn, IStream<TIn>>
+            {
+                Input = stream,
+                Predicate = predicate
+            }).Output;
+        }
+    }
+}
--- src/Paillave.Etl/StreamNodes/SelectStreamNode.cs
+++ src/Paillave.Etl/StreamNodes/SelectStreamNode.cs
@@ -8,14 +8,47 @@ using System.Text;
 
 namespace Paillave.Etl.StreamNodes
 {
+    #region Simple select
+    public interface ISelectProcessor<TIn, TOut>
+    {
+        TOut ProcessRow(TIn value);
+    }
+    public class SimpleSelectProcessor<TIn, TOut> : ISelectProcessor<TIn, TOut>
+    {
+        public Func<TIn, TOut> _selector;
+        public SimpleSelectProcessor(Func<TIn, TOut> selector)
+        {
+            _selector = selector;
+        }
+        public TOut ProcessRow(TIn value)
+        {
+            return _selector(value);
+        }
+    }
+    public class ContextSelectProcessor<TIn, TOut, TCtx> : ISelectProcessor<TIn, TOut>
+    {
+        public Func<TIn, TCtx, Action<TCtx>, TOut> _selector;
+        private TCtx _context;
+        public ContextSelectProcessor(Func<TIn, TCtx, Action<TCtx>, TOut> selector, TCtx initialContext)
+        {
+            _selector = selector;
+            _context = initialContext;
+        }
+        public TOut ProcessRow(TIn value)
+        {
+            return _selector(value, _context, SetContext);
+        }
+        private void SetContext(TCtx newContext)
+        {
+            _context = newContext;
+        }
+    }
     public class SelectArgs<TIn, TOut>
     {
         public IStream<TIn> Stream { get; set; }
-        public Func<TIn, TOut> Selector { get; set; }
-        public Func<TIn, int, TOut> IndexSelector { get; set; }
+        public ISelectProcessor<TIn, TOut> Processor { get; set; }
         public bool ExcludeNull { get; set; }
     }
-
     public class SelectStreamNode<TIn, TOut> : StreamNodeBase<TOut, IStream<TOut>, SelectArgs<TIn, TOut>>
     {
         public SelectStreamNode(string name, SelectArgs<TIn, TOut> args) : base(name, args)
@@ -24,14 +57,68 @@ namespace Paillave.Etl.StreamNodes
 
         protected override IStream<TOut> CreateOutputStream(SelectArgs<TIn, TOut> args)
         {
-            IPushObservable<TOut> obs;
-            if (args.IndexSelector == null)
-                obs = args.Stream.Observable.Map(WrapSelectForDisposal(args.Selector));
-            else
-                obs = args.Stream.Observable.Map(WrapSelectIndexForDisposal(args.IndexSelector));
+            IPushObservable<TOut> obs = args.Stream.Observable.Map(WrapSelectForDisposal<TIn, TOut>(args.Processor.ProcessRow));
             if (args.ExcludeNull)
                 obs = obs.Filter(i => i != null);
             return base.CreateUnsortedStream(obs);
         }
     }
+    #endregion
+
+    #region Select with index
+    public interface ISelectWithIndexProcessor<TIn, TOut>
+    {
+        TOut ProcessRow(TIn value, int index);
+    }
+    public class SimpleSelectWithIndexProcessor<TIn, TOut> : ISelectWithIndexProcessor<TIn, TOut>
+    {
+        public Func<TIn, int, TOut> _selector;
+        public SimpleSelectWithIndexProcessor(Func<TIn, int, TOut> selector)
+        {
+            _selector = selector;
+        }
+        public TOut ProcessRow(TIn value, int index)
+        {
+            return _selector(value, index);
+        }
+    }
+    public class ContextSelectWithIndexProcessor<TIn, TOut, TCtx> : ISelectWithIndexProcessor<TIn, TOut>
+    {
+        public Func<TIn, int, TCtx, Action<TCtx>, TOut> _selector;
+        private TCtx _context;
+        public ContextSelectWithIndexProcessor(Func<TIn, int, TCtx, Action<TCtx>, TOut> selector, TCtx initialContext)
+        {
+            _selector = selector;
+            _context = initialContext;
+        }
+        public TOut ProcessRow(TIn value, int index)
+        {
+            return _selector(value, index, _context, SetContext);
+        }
+        private void SetContext(TCtx newContext)
+        {
+            _context = newContext;
+        }
+    }
+    public class SelectWithIndexArgs<TIn, TOut>
+    {
+        public IStream<TIn> Stream { get; set; }
+        public ISelectWithIndexProcessor<TIn, TOut> Processor { get; set; }
+        public bool ExcludeNull { get; set; }
+    }
+    public class SelectWithIndexStreamNode<TIn, TOut> : StreamNodeBase<TOut, IStream<TOut>, SelectWithIndexArgs<TIn, TOut>>
+    {
+        public SelectWithIndexStreamNode(string name, SelectWithIndexArgs<TIn, TOut> args) : base(name, args)
+        {
+        }
+
+        protected override IStream<TOut> CreateOutputStream(SelectWithIndexArgs<TIn, TOut> args)
+        {
+            IPushObservable<TOut> obs = args.Stream.Observable.Map(WrapSelectIndexForDisposal<TIn, TOut>(args.Processor.ProcessRow));
+            if (args.ExcludeNull)
+                obs = obs.Filter(i => i != null);
+            return base.CreateUnsortedStream(obs);
+        }
+    }
+    #endregion
 }
