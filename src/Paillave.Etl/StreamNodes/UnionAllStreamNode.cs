ecc9ffd02e5f28e02abb8127ac34ac51e6fb61af
Royer <stephane.royer@kpmg.lu>
2018-09-25 18:17:16 +0200

    begin sync union all

--- src/Paillave.Etl/StreamNodes/UnionAllStreamNode.cs
+++ src/Paillave.Etl/StreamNodes/UnionAllStreamNode.cs
@@ -1,9 +1,11 @@
 ﻿using Paillave.Etl.Core;
 using Paillave.Etl.Core.Streams;
+using Paillave.Etl.Reactive.Core;
 using Paillave.Etl.Reactive.Operators;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
 
 namespace Paillave.Etl.StreamNodes
 {
@@ -23,4 +25,54 @@ namespace Paillave.Etl.StreamNodes
             return base.CreateUnsortedStream(args.Stream1.Observable.Concatenate(args.Stream2.Observable));
         }
     }
+
+
+
+    // public class ToSubProcessUnionAllArgs<TIn, TOut>
+    // {
+    //     public IStream<TIn> Stream { get; set; }
+    //     public IEnumerable<Func<IStream<TIn>, IStream<TOut>>> SubProcesses { get; set; }
+    //     public bool NoParallelisation { get; set; }
+    // }
+    // public class ToSubProcessUnionAllStreamNode<TIn, TOut> : StreamNodeBase<TOut, IStream<TOut>, ToSubProcessUnionAllArgs<TIn, TOut>>
+    // {
+    //     public ToSubProcessUnionAllStreamNode(string name, ToSubProcessUnionAllArgs<TIn, TOut> args) : base(name, args)
+    //     {
+    //     }
+
+    //     protected override IStream<TOut> CreateOutputStream(ToSubProcessUnionAllArgs<TIn, TOut> args)
+    //     {
+    //         Semaphore semaphore = args.NoParallelisation ? new Semaphore(1, 1) : new Semaphore(10, 10);
+    //         var outputObservable = args.Stream.Observable.First().FlatMap(i =>
+    //             {
+    //                 // IStream<TOut> outputStream = null;
+    //                 EventWaitHandle waitHandle = new EventWaitHandle(false, EventResetMode.ManualReset);
+    //                 var inputStream = new Stream<TIn>(this.Tracer, this.ExecutionContext, this.NodeName, PushObservable.FromSingle(i, waitHandle));
+    //                 var observables = new List<IPushObservable<TOut>>();
+    //                 IPushObservable<TOut> observable = null;
+    //                 foreach (var subProcess in args.SubProcesses)
+    //                 {
+    //                     var obs = subProcess(inputStream).Observable;
+    //                     if (observable == null) observable.Merge(obs);
+    //                     observables.Add(obs);
+    //                 }
+    //                 return observable;
+    //                 foreach (var subProcess in args.SubProcesses)
+    //                 {
+    //                     if (outputStream == null)
+    //                         outputStream = subProcess(inputStream);
+    //                     else
+    //                         outputStream = outputStream.Observable.Concatenate<TOut>(inputStream.Observable);
+    //                     this.ExecutionContext.AddToWaitForCompletion(this.NodeName, outputStream.Observable);
+    //                     outputStream.Observable.Subscribe(j => { }, () => semaphore.Release());
+    //                     var endOfChainObservable = new DeferedWrapperPushObservable<TOut>(outputStream.Observable, () =>
+    //                       {
+    //                           semaphore.WaitOne();
+    //                           waitHandle.Set();
+    //                       });
+    //                 }
+    //             });
+    //         return base.CreateUnsortedStream(outputObservable);
+    //     }
+    // }
 }
