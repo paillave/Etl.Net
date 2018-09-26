54dd7b737c6d6ad7d95380c1f0f8f95007096d63
Stéphane Royer <admroyer@hotmail.com>
2018-09-26 02:36:28 +0200

    union from sub processes

--- src/Paillave.Etl/StreamNodes/SubProcessesUnionStreamNode.cs
+++ src/Paillave.Etl/StreamNodes/SubProcessesUnionStreamNode.cs
@@ -3,34 +3,40 @@ using Paillave.Etl.Core.Streams;
 using Paillave.Etl.Reactive.Core;
 using Paillave.Etl.Reactive.Operators;
 using System;
+using System.Linq;
 using System.Collections.Generic;
 using System.Text;
 using System.Threading;
 
 namespace Paillave.Etl.StreamNodes
 {
-    public class ToSubProcessArgs<TIn, TOut>
+    public class SubProcessesUnionArgs<TIn, TOut>
     {
         public IStream<TIn> Stream { get; set; }
-        public Func<IStream<TIn>, IStream<TOut>> SubProcess { get; set; }
+        public IEnumerable<Func<IStream<TIn>, IStream<TOut>>> SubProcesses { get; set; }
         public bool NoParallelisation { get; set; }
     }
-    public class ToSubProcessStreamNode<TIn, TOut> : StreamNodeBase<TOut, IStream<TOut>, ToSubProcessArgs<TIn, TOut>>
+    public class SubProcessesUnionStreamNode<TIn, TOut> : StreamNodeBase<TOut, IStream<TOut>, SubProcessesUnionArgs<TIn, TOut>>
     {
-        public override bool IsAwaitable => true;
-        public ToSubProcessStreamNode(string name, ToSubProcessArgs<TIn, TOut> args) : base(name, args)
+        public SubProcessesUnionStreamNode(string name, SubProcessesUnionArgs<TIn, TOut> args) : base(name, args)
         {
         }
 
-        protected override IStream<TOut> CreateOutputStream(ToSubProcessArgs<TIn, TOut> args)
+        protected override IStream<TOut> CreateOutputStream(SubProcessesUnionArgs<TIn, TOut> args)
         {
             Semaphore semaphore = args.NoParallelisation ? new Semaphore(1, 1) : new Semaphore(10, 10);
-            var outputObservable = args.Stream.Observable.FlatMap(i =>
+            var outputObservable = args.Stream.Observable
+                .First()
+                .FlatMap(i => PushObservable.FromEnumerable(args.SubProcesses.Select(sp => new
+                {
+                    cnfg = i,
+                    subProc = sp
+                })))
+                .FlatMap(i =>
                 {
                     EventWaitHandle waitHandle = new EventWaitHandle(false, EventResetMode.ManualReset);
-                    var inputStream = new Stream<TIn>(this.Tracer, this.ExecutionContext, this.NodeName, PushObservable.FromSingle(i, waitHandle));
-                    var outputStream = args.SubProcess(inputStream);
-                    this.ExecutionContext.AddToWaitForCompletion(this.NodeName, outputStream.Observable);
+                    var inputStream = new Stream<TIn>(this.Tracer, this.ExecutionContext, this.NodeName, PushObservable.FromSingle(i.cnfg, waitHandle));
+                    var outputStream = i.subProc(inputStream);
                     outputStream.Observable.Subscribe(j => { }, () => semaphore.Release());
                     return new DeferedWrapperPushObservable<TOut>(outputStream.Observable, () =>
                     {
