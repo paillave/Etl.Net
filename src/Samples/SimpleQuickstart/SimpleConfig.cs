5b566d961ef1514a58511fb3310a76093fb67083
Stéphane Royer <admroyer@hotmail.com>
2018-09-30 17:09:34 +0200

    update documentation

---
+++ src/Samples/SimpleQuickstart/SimpleConfig.cs
@@ -0,0 +1,16 @@
+﻿using Paillave.Etl;
+using System;
+using System.IO;
+using Paillave.Etl.Core;
+using Paillave.Etl.TextFile.Core;
+using Paillave.Etl.Core.Streams;
+using Paillave.Etl.TextFile;
+
+namespace SimpleQuickstart
+{
+    public class SimpleConfig
+    {
+        public string InputFilePath { get; set; }
+        public string OutputFilePath { get; set; }
+    }
+}
