4f682056219b24d7ba4445633b51c70be38b76a5
Royer, Stephane <stephane.royer@kpmg.lu>
2018-10-31 15:41:10 +0100

    add section index to select complete sections by their occurence index

--- src/Paillave.Etl/Extensions/SelectSection.StreamEx.cs
+++ src/Paillave.Etl/Extensions/SelectSection.StreamEx.cs
@@ -54,5 +54,58 @@ namespace Paillave.Etl.Extensions
                 SwitchToIgnore = switchToIgnore
             }).Output;
         }
+
+
+
+
+
+
+
+
+
+
+
+
+
+        public static IStream<TOut> SelectSection<TIn, TOut>(this IStream<TIn> stream, string name, KeepingState initialState, Func<TIn, SwitchBehavior> switcher, Func<TIn, int, TOut> resultSelector)
+        {
+            return new SelectSectionStreamNode<TIn, TOut>(name, new SelectSectionArgs<TIn, TOut>
+            {
+                Stream = stream,
+                InitialState = initialState,
+                SwitchToKeep = switcher,
+                ResultSelector = resultSelector
+            }).Output;
+        }
+        public static IStream<TOut> SelectSection<TIn, TOut>(this IStream<TIn> stream, string name, Func<TIn, SwitchBehavior> switcher, Func<TIn, int, TOut> resultSelector)
+        {
+            return new SelectSectionStreamNode<TIn, TOut>(name, new SelectSectionArgs<TIn, TOut>
+            {
+                Stream = stream,
+                SwitchToKeep = switcher,
+                ResultSelector = resultSelector
+            }).Output;
+        }
+        public static IStream<TOut> SelectSection<TIn, TOut>(this IStream<TIn> stream, string name, KeepingState initialState, Func<TIn, SwitchBehavior> switchToKeep, Func<TIn, SwitchBehavior> switchToIgnore, Func<TIn, int, TOut> resultSelector)
+        {
+            return new SelectSectionStreamNode<TIn, TOut>(name, new SelectSectionArgs<TIn, TOut>
+            {
+                Stream = stream,
+                InitialState = initialState,
+                SwitchToKeep = switchToKeep,
+                SwitchToIgnore = switchToIgnore,
+                ResultSelector = resultSelector
+            }).Output;
+        }
+        public static IStream<TOut> SelectSection<TIn, TOut>(this IStream<TIn> stream, string name, Func<TIn, SwitchBehavior> switchToKeep, Func<TIn, SwitchBehavior> switchToIgnore, Func<TIn, int, TOut> resultSelector)
+        {
+            return new SelectSectionStreamNode<TIn, TOut>(name, new SelectSectionArgs<TIn, TOut>
+            {
+                Stream = stream,
+                SwitchToKeep = switchToKeep,
+                SwitchToIgnore = switchToIgnore,
+                ResultSelector = resultSelector
+            }).Output;
+        }
     }
 }
