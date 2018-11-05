d8c4dcb085a9f75a269fbe8eb6607270e536b565
Royer, Stephane <stephane.royer@kpmg.lu>
2018-11-05 16:42:33 +0100

    rest

---
+++ src/Paillave.Etl.Rest/RestRepositorySetup.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Paillave.Etl.Rest
+{
+    public class RestRepositorySetup
+    {
+        public string BaseAddress { get; }
+        public virtual bool UseDefaultCredentials { get; } = false;
+        public RestRepositorySetup(string baseAddress)
+        {
+            this.BaseAddress = baseAddress;
+        }
+        public virtual AuthenticationHeaderValue GetAuthenticationHeader()
+        {
+            return null;
+        }
+    }
+}
