93f49dd6e3e211b0e1361c5d2444ad81737b0220
Stéphane Royer <admroyer@hotmail.com>
2018-11-19 00:15:43 +0100

    first and last

---
+++ src/Paillave.Etl/StreamNodes/FirstStreamNode.cs
@@ -0,0 +1,25 @@
+﻿using Paillave.Etl.Core;
+using Paillave.Etl.Core.Streams;
+using Paillave.Etl.Reactive.Operators;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Paillave.Etl.StreamNodes
+{
+    public class FirstArgs<TOut>
+    {
+        public IStream<TOut> Input { get; set; }
+    }
+    public class FirstStreamNode<TOut> : StreamNodeBase<TOut, ISingleStream<TOut>, FirstArgs<TOut>>
+    {
+        public FirstStreamNode(string name, FirstArgs<TOut> args) : base(name, args)
+        {
+        }
+
+        protected override ISingleStream<TOut> CreateOutputStream(FirstArgs<TOut> args)
+        {
+            return base.CreateSingleStream(args.Input.Observable.First());
+        }
+    }
+}
---
+++ src/Paillave.Etl/StreamNodes/LastStreamNode.cs
@@ -0,0 +1,25 @@
+﻿using Paillave.Etl.Core;
+using Paillave.Etl.Core.Streams;
+using Paillave.Etl.Reactive.Operators;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Paillave.Etl.StreamNodes
+{
+    public class LastArgs<TOut>
+    {
+        public IStream<TOut> Input { get; set; }
+    }
+    public class LastStreamNode<TOut> : StreamNodeBase<TOut, ISingleStream<TOut>, LastArgs<TOut>>
+    {
+        public LastStreamNode(string name, LastArgs<TOut> args) : base(name, args)
+        {
+        }
+
+        protected override ISingleStream<TOut> CreateOutputStream(LastArgs<TOut> args)
+        {
+            return base.CreateSingleStream(args.Input.Observable.Last());
+        }
+    }
+}
