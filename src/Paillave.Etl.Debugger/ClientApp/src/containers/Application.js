c2a98965b1e30577e322125cd8110d94e9c3ecab
Royer, Stephane <stephane.royer@kpmg.lu>
2018-12-27 18:53:39 +0100

    remove debug project

--- src/Paillave.Etl.Debugger/ClientApp/src/containers/Application.js
+++
@@ -1,9 +0,0 @@
-import { bindActionCreators } from 'redux';
-import { connect } from 'react-redux';
-import { actionCreators } from '../store/Application';
-import ApplicationComponent from '../components/Application';
-
-export default connect(
-    state => state.app,
-    dispatch => bindActionCreators(actionCreators, dispatch)
-)(ApplicationComponent);
--- src/Paillave.Etl.Debugger/ClientApp/src/containers/NodeTracesHeaders.js
+++
@@ -1,9 +0,0 @@
-import { bindActionCreators } from 'redux';
-import { connect } from 'react-redux';
-import { actionCreators } from '../store/Application';
-import NodeTracesHeaders from '../components/NodeTracesHeaders';
-
-export default connect(
-    state => state.app,
-    dispatch => bindActionCreators(actionCreators, dispatch)
-)(NodeTracesHeaders);
