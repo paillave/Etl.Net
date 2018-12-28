b393bd362ed6f57e2d516d0bf093efc93a33ced2
Stéphane Royer <admroyer@hotmail.com>
2018-12-28 02:22:09 +0100

    sftp extension

---
+++ src/Paillave.Etl.Sftp/SftpUploadException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+
+namespace Paillave.Etl.Sftp
+{
+    public class SftpUploadException:Exception
+    {
+        // public SftpUploadException(SftpStatusCode statusCode, string statusDescription) : base($"failed to upload. {statusCode}: {statusDescription}")
+        // {
+
+        // }
+    }
+}
