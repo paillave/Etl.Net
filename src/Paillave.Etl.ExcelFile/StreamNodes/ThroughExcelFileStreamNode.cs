4948a77b347588183a3edb4ea3885a5757f4b574
Royer, Stephane <stephane.royer@kpmg.lu>
2019-02-01 16:00:59 +0100

    code clean

--- src/Paillave.Etl.ExcelFile/StreamNodes/ThroughExcelFileStreamNode.cs
+++ src/Paillave.Etl.ExcelFile/StreamNodes/ThroughExcelFileStreamNode.cs
@@ -18,8 +18,8 @@ namespace Paillave.Etl.ExcelFile.StreamNodes
         where TStream : IStream<TIn>
     {
         public TStream MainStream { get; set; }
-        public ISingleStream<Stream> TargetStream { get; set; }
         public ExcelFileDefinition<TIn> Mapping { get; set; }
+        public ISingleStream<Stream> TargetStream { get; set; }
     }
     public class ThroughExcelFileStreamNode<TIn, TStream> : StreamNodeBase<TIn, TStream, ThroughExcelFileArgs<TIn, TStream>>
         where TStream : IStream<TIn>
@@ -33,44 +33,12 @@ namespace Paillave.Etl.ExcelFile.StreamNodes
             var firstStreamWriter = args.TargetStream.Observable.First().DelayTillEndOfStream();
             var obs = args.MainStream.Observable.ToList()
                 .CombineWithLatest(firstStreamWriter, (i, r) => { ProcessValueToOutput(r, i); return i; }, true)
-                //.CombineWithLatest(firstStreamWriter, (i, r) => { ProcessValueToOutput(r, args.Mapping, i); return i; }, true)
                 .FlatMap(i => PushObservable.FromEnumerable(i));
             return CreateMatchingStream(obs, args.MainStream);
         }
-        //protected void ProcessValueToOutput(SystemIO.StreamWriter streamWriter, ExcelFileDefinition<TIn> mapping, IList<TIn> value)
         protected void ProcessValueToOutput(Stream streamWriter, IList<TIn> value)
         {
-            var pck = new ExcelPackage();
-            var style = pck.Workbook.Styles.CreateNamedStyle("Date");
-            style.Style.Numberformat.Format = "dd/MM/yyyy";
-            var wsList = pck.Workbook.Worksheets.Add("Sheet1");
-            if (base.Args.Mapping != null)
-            {
-                var excelColumns = base.Args.Mapping.GetExcelReader().PropertySetters.OrderBy(i => i.Key).Select(i => i.Value).ToArray();
-                var r1 = wsList.Cells["A1"].LoadFromCollection(value, true, TableStyles.Medium11, BindingFlags.Instance | BindingFlags.Public, excelColumns.Select(i => i.PropertyInfo).ToArray());
-                var table = wsList.Tables.GetFromRange(r1);
-                foreach (var item in excelColumns.Select((i, idx) => new { ColumnIndex = idx, Label = i.ColumnName ?? i.PropertyInfo.Name, Type = i.PropertyInfo.PropertyType }))
-                {
-                    table.Columns[item.ColumnIndex].Name = item.Label;
-                    if (item.Type == typeof(DateTime))
-                        table.Columns[item.ColumnIndex].DataCellStyleName = "Date";
-                }
-                r1.AutoFitColumns();
-            }
-            else
-            {
-                var properties = typeof(TIn).GetProperties();
-                var r1 = wsList.Cells["A1"].LoadFromCollection(value, true, TableStyles.Medium11, BindingFlags.Instance | BindingFlags.Public, properties);
-                var table = wsList.Tables.GetFromRange(r1);
-                foreach (var item in properties.Select((p, i) => new { Index = i, PropertyInfo = p }).Where(i => i.PropertyInfo.PropertyType == typeof(DateTime)))
-                {
-                    table.Columns[item.Index].DataCellStyleName = "Date";
-                }
-
-                r1.AutoFitColumns();
-            }
-            BinaryWriter bw = new BinaryWriter(streamWriter);
-            bw.Write(pck.GetAsByteArray());
+            value.WriteExcelListInStream(Args.Mapping, streamWriter);
         }
     }
 }
