3521f522b28776028c575e4556f168e6159c0605
Stephane Royer <stephane.royer@fundprocess.lu>
2022-01-11 12:13:10 +0100

    bloomberg response parser

--- src/Tutorials/SimpleTutorial/Program.cs
+++ src/Tutorials/SimpleTutorial/Program.cs
@@ -14,21 +14,44 @@ namespace SimpleTutorial
     {
         static async Task Main(string[] args)
         {
-            var processRunner = StreamProcessRunner.Create<string>(DefineProcess);
+            var processRunner = StreamProcessRunner.Create<string>(DefineProcess1);
             processRunner.DebugNodeStream += (sender, e) => { };
-            using (var cnx = new SqlConnection(args[1]))
-            {
-                cnx.Open();
-                var executionOptions = new ExecutionOptions<string>
-                {
-                    Resolver = new SimpleDependencyResolver().Register(cnx),
-                };
-                var res = await processRunner.ExecuteAsync(args[0], executionOptions);
-                Console.Write(res.Failed ? "Failed" : "Succeeded");
-                if (res.Failed)
-                    Console.Write($"{res.ErrorTraceEvent.NodeName}({res.ErrorTraceEvent.NodeTypeName}):{res.ErrorTraceEvent.Content.Message}");
-            }
+            var res = await processRunner.ExecuteAsync(args[0]);
+            Console.Write(res.Failed ? "Failed" : "Succeeded");
+            if (res.Failed)
+                Console.Write($"{res.ErrorTraceEvent.NodeName}({res.ErrorTraceEvent.NodeTypeName}):{res.ErrorTraceEvent.Content.Message}");
         }
+        private static void DefineProcess1(ISingleStream<string> contextStream)
+        {
+            contextStream
+                .CrossApplyFolderFiles("get files", "*.txt")
+                .CrossApply("parse file", BloombergValuesProvider.Create(FlatFileDefinition.Create(i => new
+                {
+                    CountryFullName = i.ToColumn("COUNTRY_FULL_NAME"),
+                    IndustrySector = i.ToColumn("INDUSTRY_SECTOR"),
+                    IndustrySectorNum = i.ToColumn("INDUSTRY_SECTOR_NUM"),
+                    EqyShOut = i.ToNumberColumn<double?>("EQY_SH_OUT", "."),
+                    EqyShOutActual = i.ToNumberColumn<double?>("EQY_SH_OUT_ACTUAL", "."),
+                    EqyFloat = i.ToNumberColumn<double?>("EQY_FLOAT", "."),
+                    CrncyAdjMktCap = i.ToNumberColumn<double?>("CRNCY_ADJ_MKT_CAP", "."),
+                    HisVolOnPx1 = i.ToNumberColumn<double?>("HIS_VOL_ON_PX:1", "."),
+                    HisVolOnPx2 = i.ToNumberColumn<double?>("HIS_VOL_ON_PX:2", "."),
+                    MhisCloseOnPx3 = i.ToNumberColumn<double?>("MHIS_CLOSE_ON_PX:3", "."),
+                    MhisCloseOnPx4 = i.ToNumberColumn<double?>("MHIS_CLOSE_ON_PX:4", "."),
+                    CountryIso = i.ToColumn("COUNTRY_ISO"),
+                    DvdExDt = i.ToOptionalDateColumn("DVD_EX_DT", "MM/dd/yyyy"),
+                    DvdFreq = i.ToColumn("DVD_FREQ"),
+                    EqyDvdRightExDtCurr = i.ToColumn("EQY_DVD_RIGHT_EX_DT_CURR"),
+                    SecurityTyp = i.ToColumn("SECURITY_TYP"),
+                    PeRatio = i.ToNumberColumn<double?>("PE_RATIO", "."),
+                    EqyDvdYld12m = i.ToNumberColumn<double?>("EQY_DVD_YLD_12M", "."),
+                })))
+                .Do("write to console", i =>
+                {
+
+                });
+        }
+
         private static void DefineProcess(ISingleStream<string> contextStream)
         {
             contextStream
