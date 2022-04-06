913bb32ca4067a55d4053bacd8aa8a7e68ceae85
Stephane Royer <stephane.royer@fundprocess.lu>
2022-04-06 23:10:29 +0200

    debug tools

--- src/Tutorials/Paillave.Etl.Samples/Program.cs
+++ src/Tutorials/Paillave.Etl.Samples/Program.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Threading.Tasks;
+using Paillave.EntityFrameworkCoreExtension.Core;
 using Paillave.Etl.Core;
 using Paillave.Etl.Ftp;
+using Paillave.Etl.Samples.DataAccess;
 using Paillave.Pdf;
 
 namespace Paillave.Etl.Samples
@@ -12,16 +14,22 @@ namespace Paillave.Etl.Samples
     {
         static async Task Main(string[] args)
         {
-            var processRunner = StreamProcessRunner.Create<string[]>(Import);
-            var res = await processRunner.ExecuteAsync(args);
+            using (var dbCtx = new TestDbContext())
+            {
+                await dbCtx.Set<Position>().DeleteWhereAsync(i => i.CompositionId == 10);
+            }
+            // var processRunner = StreamProcessRunner.Create<string[]>(Import);
+            // var res = await processRunner.ExecuteAsync(args);
         }
         public static void Import(ISingleStream<string[]> contextStream)
         {
-            var portfolioFileStream = contextStream.CrossApply("Get Files", new FtpFileValueProvider("SRC", "Solar exports", "files from ftp", new FtpAdapterConnectionParameters{
-                Server="localhost",
-                Login="stephane",
-                Password="xxxxxxxx"
-            }, new FtpAdapterProviderParameters{
+            var portfolioFileStream = contextStream.CrossApply("Get Files", new FtpFileValueProvider("SRC", "Solar exports", "files from ftp", new FtpAdapterConnectionParameters
+            {
+                Server = "localhost",
+                Login = "stephane",
+                Password = "xxxxxxxx"
+            }, new FtpAdapterProviderParameters
+            {
 
             }))
         .Do("print files to console", i => Console.WriteLine(i.Name));
