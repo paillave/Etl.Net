b50285598acfd335ab9bc16aac6a15717b29cd0d
Stéphane Royer <stephane.royer@fundprocess.lu>
2022-06-13 17:55:02 +0200

    start scheduler

--- src/Paillave.Etl.Scheduler/Program.cs
+++ src/Paillave.Etl.Scheduler/Program.cs
@@ -1,1 +1,11 @@
 ﻿// https://codeburst.io/schedule-cron-jobs-using-hostedservice-in-asp-net-core-e17c47ba06
+using Paillave.Etl.Scheduler;
+Console.WriteLine("Starting...");
+using (var emitter = TickEmitter.Create(new { CronExpression = @"@every_second", BatchId = 15 }, i => i.CronExpression))
+using (var subscription = emitter.Subscribe(i => Console.WriteLine(i.BatchId)))
+{
+    emitter.Start();
+    Thread.Sleep(5000);
+    emitter.UpdateEmitter(new { CronExpression = "* * * * *", BatchId = 16 });
+    Thread.Sleep(3 * 60 * 1000);
+}
---
+++ src/Paillave.Etl.Scheduler/TickEmitter.cs
@@ -0,0 +1,145 @@
+﻿// https://codeburst.io/schedule-cron-jobs-using-hostedservice-in-asp-net-core-e17c47ba06
+using Cronos;
+
+namespace Paillave.Etl.Scheduler;
+public class TickEmitter : TickEmitter<string>
+{
+    public TickEmitter(string cronExpression) : base(cronExpression, i => i) { }
+    public static TickEmitter<TEmitter> Create<TEmitter>(TEmitter sourceMetadata, Func<TEmitter, string> getCronExpression) => new TickEmitter<TEmitter>(sourceMetadata, getCronExpression);
+}
+public class TickEmitter<TEmitter> : IDisposable
+{
+    private class TickRunningContext : IDisposable
+    {
+        private CancellationTokenSource _internalCancellationTokenSource;
+        private CancellationTokenSource _combinedCancellationTokenSource;
+        // private System.Timers.Timer? _timer = null;
+        public TickRunningContext(CancellationToken externalCancellationToken, Action onStop)
+        {
+            _internalCancellationTokenSource = new CancellationTokenSource();
+            _combinedCancellationTokenSource = CancellationTokenSource.CreateLinkedTokenSource(externalCancellationToken, _internalCancellationTokenSource.Token);
+        }
+        public CancellationToken CancellationToken => _combinedCancellationTokenSource.Token;
+
+        public void Dispose()
+        {
+            _internalCancellationTokenSource.Dispose();
+            _combinedCancellationTokenSource.Dispose();
+        }
+        public void Stop()
+        {
+            _internalCancellationTokenSource.Cancel();
+        }
+    }
+    private class TickSubscription : IDisposable
+    {
+        public TickSubscription(Action<TEmitter> push, Action<TickSubscription> unsubscribe) => (Push, _unsubscribe) = (push, unsubscribe);
+        public void Dispose() => _unsubscribe(this);
+        private readonly Action<TickSubscription> _unsubscribe;
+        public Action<TEmitter> Push { get; }
+    }
+    public void UpdateEmitter(TEmitter? emitter = default)
+    {
+        emitter ??= this.Emitter;
+        lock (this._syncObject)
+        {
+            this.Emitter = emitter;
+            if (Running) Restart();
+        }
+    }
+    private void Restart()
+    {
+        lock (_syncObject)
+        {
+            if (_timer != null && this._runningContext != null)
+            {
+                _timer.Stop();
+                Task.Run(() => this.ScheduleNext(this._runningContext.CancellationToken), this._runningContext.CancellationToken);
+            }
+        }
+    }
+    private bool disposedValue;
+    private readonly object _syncObject = new Object();
+    private System.Timers.Timer? _timer = null;
+    public TEmitter Emitter { get; private set; }
+    private readonly List<TickSubscription> _subscriptions = new List<TickSubscription>();
+    private Func<TEmitter, string> _getCronExpression;
+    private TickRunningContext? _runningContext = null;
+    public bool Running => _runningContext != null;
+    public TickEmitter(TEmitter sourceMetadata, Func<TEmitter, string> getCronExpression)
+        => (Emitter, _getCronExpression) = (sourceMetadata, getCronExpression);
+    public IDisposable Subscribe(Action<TEmitter> push)
+    {
+        var tickSubscription = new TickSubscription(push, ts => _subscriptions.Remove(ts));
+        _subscriptions.Add(tickSubscription);
+        return tickSubscription;
+    }
+    public void Start() => Start(CancellationToken.None);
+
+    public void Start(CancellationToken cancellationToken)
+    {
+        lock (_syncObject)
+        {
+            if (Running) throw new Exception("Running already");
+            this._runningContext = new TickRunningContext(cancellationToken, this.Stop);
+            Task.Run(() => this.ScheduleNext(this._runningContext.CancellationToken), this._runningContext.CancellationToken);
+        }
+    }
+    private void ScheduleNext(CancellationToken cancellationToken)
+    {
+        lock (this._syncObject)
+        {
+            var next = CronExpression.Parse(this._getCronExpression(this.Emitter)).GetNextOccurrence(DateTimeOffset.Now, TimeZoneInfo.Local);
+            if (next == null) return;
+            var totalMilliseconds = (next.Value - DateTimeOffset.Now).TotalMilliseconds;
+            _timer = new System.Timers.Timer(Math.Max(totalMilliseconds, 0));
+        }
+        _timer.Elapsed += (sender, args) =>
+           {
+               _timer.Stop();
+               _timer.Dispose();
+               _timer = null;
+               _subscriptions.ForEach(subscription => subscription.Push(this.Emitter));
+               this.ScheduleNext(cancellationToken);
+           };
+        _timer.Start();
+    }
+    public void Stop()
+    {
+        lock (_syncObject)
+        {
+            if (this._runningContext == null) throw new Exception("Not running yet");
+            this._runningContext.Stop();
+            this._runningContext.Dispose();
+            this._runningContext = null;
+            this._timer?.Stop();
+        }
+    }
+    protected virtual void Dispose(bool disposing)
+    {
+        if (!disposedValue)
+        {
+            if (disposing)
+            {
+                // TODO: dispose managed state (managed objects)
+            }
+            if (this._runningContext != null)
+            {
+                this._runningContext.Stop();
+                this._runningContext.Dispose();
+                this._runningContext = null;
+            }
+            if (this._timer != null)
+            {
+                this._timer.Stop();
+                this._timer.Dispose();
+            }
+            disposedValue = true;
+        }
+    }
+    public void Dispose()
+    {
+        Dispose(disposing: true);
+        GC.SuppressFinalize(this);
+    }
+}
