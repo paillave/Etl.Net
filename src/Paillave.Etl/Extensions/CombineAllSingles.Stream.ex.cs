48078091c29315096f22a7bb7be70898e68017e1
Stephane Royer <stephane.royer@fundprocess.lu>
2022-07-30 19:22:16 +0200

    improve combine single streams

--- src/Paillave.Etl/Extensions/CombineAllSingles.Stream.ex.cs
+++ src/Paillave.Etl/Extensions/CombineAllSingles.Stream.ex.cs
@@ -4,17 +4,17 @@ namespace Paillave.Etl.Core
 {
     public static partial class CombineAllSinglesEx
     {
-        public static ISingleStream<Tuple<TIn1, TIn2>> Combine<TIn1, TIn2>(this ISingleStream<TIn1> stream1, string name, ISingleStream<TIn2> stream2)
-            => new CombineAllSinglesStreamNode<TIn1, TIn2>(name, new Tuple<ISingleStream<TIn1>, ISingleStream<TIn2>>(stream1, stream2)).Output;
-        public static ISingleStream<Tuple<TIn1, TIn2, TIn3>> Combine<TIn1, TIn2, TIn3>(this ISingleStream<TIn1> stream1, string name, ISingleStream<TIn2> stream2, ISingleStream<TIn3> stream3)
-            => new CombineAllSinglesStreamNode<TIn1, TIn2, TIn3>(name, new Tuple<ISingleStream<TIn1>, ISingleStream<TIn2>, ISingleStream<TIn3>>(stream1, stream2, stream3)).Output;
-        public static ISingleStream<Tuple<TIn1, TIn2, TIn3, TIn4>> Combine<TIn1, TIn2, TIn3, TIn4>(this ISingleStream<TIn1> stream1, string name, ISingleStream<TIn2> stream2, ISingleStream<TIn3> stream3, ISingleStream<TIn4> stream4)
-            => new CombineAllSinglesStreamNode<TIn1, TIn2, TIn3, TIn4>(name, new Tuple<ISingleStream<TIn1>, ISingleStream<TIn2>, ISingleStream<TIn3>, ISingleStream<TIn4>>(stream1, stream2, stream3, stream4)).Output;
-        public static ISingleStream<Tuple<TIn1, TIn2, TIn3, TIn4, TIn5>> Combine<TIn1, TIn2, TIn3, TIn4, TIn5>(this ISingleStream<TIn1> stream1, string name, ISingleStream<TIn2> stream2, ISingleStream<TIn3> stream3, ISingleStream<TIn4> stream4, ISingleStream<TIn5> stream5)
-            => new CombineAllSinglesStreamNode<TIn1, TIn2, TIn3, TIn4, TIn5>(name, new Tuple<ISingleStream<TIn1>, ISingleStream<TIn2>, ISingleStream<TIn3>, ISingleStream<TIn4>, ISingleStream<TIn5>>(stream1, stream2, stream3, stream4, stream5)).Output;
-        public static ISingleStream<Tuple<TIn1, TIn2, TIn3, TIn4, TIn5, TIn6>> Combine<TIn1, TIn2, TIn3, TIn4, TIn5, TIn6>(this ISingleStream<TIn1> stream1, string name, ISingleStream<TIn2> stream2, ISingleStream<TIn3> stream3, ISingleStream<TIn4> stream4, ISingleStream<TIn5> stream5, ISingleStream<TIn6> stream6)
-            => new CombineAllSinglesStreamNode<TIn1, TIn2, TIn3, TIn4, TIn5, TIn6>(name, new Tuple<ISingleStream<TIn1>, ISingleStream<TIn2>, ISingleStream<TIn3>, ISingleStream<TIn4>, ISingleStream<TIn5>, ISingleStream<TIn6>>(stream1, stream2, stream3, stream4, stream5, stream6)).Output;
-        public static ISingleStream<Tuple<TIn1, TIn2, TIn3, TIn4, TIn5, TIn6, TIn7>> Combine<TIn1, TIn2, TIn3, TIn4, TIn5, TIn6, TIn7>(this ISingleStream<TIn1> stream1, string name, ISingleStream<TIn2> stream2, ISingleStream<TIn3> stream3, ISingleStream<TIn4> stream4, ISingleStream<TIn5> stream5, ISingleStream<TIn6> stream6, ISingleStream<TIn7> stream7)
-            => new CombineAllSinglesStreamNode<TIn1, TIn2, TIn3, TIn4, TIn5, TIn6, TIn7>(name, new Tuple<ISingleStream<TIn1>, ISingleStream<TIn2>, ISingleStream<TIn3>, ISingleStream<TIn4>, ISingleStream<TIn5>, ISingleStream<TIn6>, ISingleStream<TIn7>>(stream1, stream2, stream3, stream4, stream5, stream6, stream7)).Output;
+        public static ISingleStream<TOut> Combine<TIn1, TIn2, TOut>(this ISingleStream<TIn1> stream1, string name, ISingleStream<TIn2> stream2, Func<TIn1, TIn2, TOut> map)
+            => new CombineAllSinglesStreamNode<TIn1, TIn2, TOut>(name, (stream1, stream2, map)).Output;
+        public static ISingleStream<TOut> Combine<TIn1, TIn2, TIn3, TOut>(this ISingleStream<TIn1> stream1, string name, ISingleStream<TIn2> stream2, ISingleStream<TIn3> stream3, Func<TIn1, TIn2, TIn3, TOut> map)
+            => new CombineAllSinglesStreamNode<TIn1, TIn2, TIn3, TOut>(name, (stream1, stream2, stream3, map)).Output;
+        public static ISingleStream<TOut> Combine<TIn1, TIn2, TIn3, TIn4, TOut>(this ISingleStream<TIn1> stream1, string name, ISingleStream<TIn2> stream2, ISingleStream<TIn3> stream3, ISingleStream<TIn4> stream4, Func<TIn1, TIn2, TIn3, TIn4, TOut> map)
+            => new CombineAllSinglesStreamNode<TIn1, TIn2, TIn3, TIn4, TOut>(name, (stream1, stream2, stream3, stream4, map)).Output;
+        public static ISingleStream<TOut> Combine<TIn1, TIn2, TIn3, TIn4, TIn5, TOut>(this ISingleStream<TIn1> stream1, string name, ISingleStream<TIn2> stream2, ISingleStream<TIn3> stream3, ISingleStream<TIn4> stream4, ISingleStream<TIn5> stream5, Func<TIn1, TIn2, TIn3, TIn4, TIn5, TOut> map)
+            => new CombineAllSinglesStreamNode<TIn1, TIn2, TIn3, TIn4, TIn5, TOut>(name, (stream1, stream2, stream3, stream4, stream5, map)).Output;
+        public static ISingleStream<TOut> Combine<TIn1, TIn2, TIn3, TIn4, TIn5, TIn6, TOut>(this ISingleStream<TIn1> stream1, string name, ISingleStream<TIn2> stream2, ISingleStream<TIn3> stream3, ISingleStream<TIn4> stream4, ISingleStream<TIn5> stream5, ISingleStream<TIn6> stream6, Func<TIn1, TIn2, TIn3, TIn4, TIn5, TIn6, TOut> map)
+            => new CombineAllSinglesStreamNode<TIn1, TIn2, TIn3, TIn4, TIn5, TIn6, TOut>(name, (stream1, stream2, stream3, stream4, stream5, stream6, map)).Output;
+        public static ISingleStream<TOut> Combine<TIn1, TIn2, TIn3, TIn4, TIn5, TIn6, TIn7, TOut>(this ISingleStream<TIn1> stream1, string name, ISingleStream<TIn2> stream2, ISingleStream<TIn3> stream3, ISingleStream<TIn4> stream4, ISingleStream<TIn5> stream5, ISingleStream<TIn6> stream6, ISingleStream<TIn7> stream7, Func<TIn1, TIn2, TIn3, TIn4, TIn5, TIn6, TIn7, TOut> map)
+            => new CombineAllSinglesStreamNode<TIn1, TIn2, TIn3, TIn4, TIn5, TIn6, TIn7, TOut>(name, (stream1, stream2, stream3, stream4, stream5, stream6, stream7, map)).Output;
     }
 }
--- src/Paillave.Etl/Extensions/StreamNodes/CombineAllSinglesStreamNode.cs
+++ src/Paillave.Etl/Extensions/StreamNodes/CombineAllSinglesStreamNode.cs
@@ -3,72 +3,72 @@ using Paillave.Etl.Reactive.Operators;
 
 namespace Paillave.Etl.Core
 {
-    public class CombineAllSinglesStreamNode<TIn1, TIn2> : StreamNodeBase<Tuple<TIn1, TIn2>, ISingleStream<Tuple<TIn1, TIn2>>, Tuple<ISingleStream<TIn1>, ISingleStream<TIn2>>>
+    public class CombineAllSinglesStreamNode<TIn1, TIn2, TOut> : StreamNodeBase<TOut, ISingleStream<TOut>, (ISingleStream<TIn1>, ISingleStream<TIn2>, Func<TIn1, TIn2, TOut> map)>
     {
-        public CombineAllSinglesStreamNode(string name, Tuple<ISingleStream<TIn1>, ISingleStream<TIn2>> args) : base(name, args) { }
+        public CombineAllSinglesStreamNode(string name, (ISingleStream<TIn1>, ISingleStream<TIn2>, Func<TIn1, TIn2, TOut> map) args) : base(name, args) { }
         public override ProcessImpact PerformanceImpact => ProcessImpact.Light;
         public override ProcessImpact MemoryFootPrint => ProcessImpact.Light;
-        protected override ISingleStream<Tuple<TIn1, TIn2>> CreateOutputStream(Tuple<ISingleStream<TIn1>, ISingleStream<TIn2>> args)
-            => base.CreateSingleStream(args.Item1.Observable.CombineWithLatest(args.Item2.Observable, (i, j) => new Tuple<TIn1, TIn2>(i, j)));
+        protected override ISingleStream<TOut> CreateOutputStream((ISingleStream<TIn1>, ISingleStream<TIn2>, Func<TIn1, TIn2, TOut> map) args)
+            => base.CreateSingleStream(args.Item1.Observable.CombineWithLatest(args.Item2.Observable, (i, j) => args.map(i, j)));
     }
-    public class CombineAllSinglesStreamNode<TIn1, TIn2, TIn3> : StreamNodeBase<Tuple<TIn1, TIn2, TIn3>, ISingleStream<Tuple<TIn1, TIn2, TIn3>>, Tuple<ISingleStream<TIn1>, ISingleStream<TIn2>, ISingleStream<TIn3>>>
+    public class CombineAllSinglesStreamNode<TIn1, TIn2, TIn3, TOut> : StreamNodeBase<TOut, ISingleStream<TOut>, (ISingleStream<TIn1>, ISingleStream<TIn2>, ISingleStream<TIn3>, Func<TIn1, TIn2, TIn3, TOut> map)>
     {
-        public CombineAllSinglesStreamNode(string name, Tuple<ISingleStream<TIn1>, ISingleStream<TIn2>, ISingleStream<TIn3>> args) : base(name, args) { }
+        public CombineAllSinglesStreamNode(string name, (ISingleStream<TIn1>, ISingleStream<TIn2>, ISingleStream<TIn3>, Func<TIn1, TIn2, TIn3, TOut> map) args) : base(name, args) { }
         public override ProcessImpact PerformanceImpact => ProcessImpact.Light;
         public override ProcessImpact MemoryFootPrint => ProcessImpact.Light;
-        protected override ISingleStream<Tuple<TIn1, TIn2, TIn3>> CreateOutputStream(Tuple<ISingleStream<TIn1>, ISingleStream<TIn2>, ISingleStream<TIn3>> args)
+        protected override ISingleStream<TOut> CreateOutputStream((ISingleStream<TIn1>, ISingleStream<TIn2>, ISingleStream<TIn3>, Func<TIn1, TIn2, TIn3, TOut> map) args)
             => base.CreateSingleStream(args.Item1.Observable
-                .CombineWithLatest(args.Item2.Observable, (i, j) => new Tuple<TIn1, TIn2>(i, j))
-                    .CombineWithLatest(args.Item3.Observable, (i, j) => new Tuple<TIn1, TIn2, TIn3>(i.Item1, i.Item2, j)));
+                .CombineWithLatest(args.Item2.Observable, (i, j) => (i, j))
+                    .CombineWithLatest(args.Item3.Observable, (i, j) => args.map(i.Item1, i.Item2, j)));
     }
-    public class CombineAllSinglesStreamNode<TIn1, TIn2, TIn3, TIn4> : StreamNodeBase<Tuple<TIn1, TIn2, TIn3, TIn4>, ISingleStream<Tuple<TIn1, TIn2, TIn3, TIn4>>, Tuple<ISingleStream<TIn1>, ISingleStream<TIn2>, ISingleStream<TIn3>, ISingleStream<TIn4>>>
+    public class CombineAllSinglesStreamNode<TIn1, TIn2, TIn3, TIn4, TOut> : StreamNodeBase<TOut, ISingleStream<TOut>, (ISingleStream<TIn1>, ISingleStream<TIn2>, ISingleStream<TIn3>, ISingleStream<TIn4>, Func<TIn1, TIn2, TIn3, TIn4, TOut> map)>
     {
-        public CombineAllSinglesStreamNode(string name, Tuple<ISingleStream<TIn1>, ISingleStream<TIn2>, ISingleStream<TIn3>, ISingleStream<TIn4>> args) : base(name, args) { }
+        public CombineAllSinglesStreamNode(string name, (ISingleStream<TIn1>, ISingleStream<TIn2>, ISingleStream<TIn3>, ISingleStream<TIn4>, Func<TIn1, TIn2, TIn3, TIn4, TOut> map) args) : base(name, args) { }
         public override ProcessImpact PerformanceImpact => ProcessImpact.Light;
         public override ProcessImpact MemoryFootPrint => ProcessImpact.Light;
-        protected override ISingleStream<Tuple<TIn1, TIn2, TIn3, TIn4>> CreateOutputStream(Tuple<ISingleStream<TIn1>, ISingleStream<TIn2>, ISingleStream<TIn3>, ISingleStream<TIn4>> args)
+        protected override ISingleStream<TOut> CreateOutputStream((ISingleStream<TIn1>, ISingleStream<TIn2>, ISingleStream<TIn3>, ISingleStream<TIn4>, Func<TIn1, TIn2, TIn3, TIn4, TOut> map) args)
             => base.CreateSingleStream(args.Item1.Observable
-                .CombineWithLatest(args.Item2.Observable, (i, j) => new Tuple<TIn1, TIn2>(i, j))
-                    .CombineWithLatest(args.Item3.Observable, (i, j) => new Tuple<TIn1, TIn2, TIn3>(i.Item1, i.Item2, j))
-                        .CombineWithLatest(args.Item4.Observable, (i, j) => new Tuple<TIn1, TIn2, TIn3, TIn4>(i.Item1, i.Item2, i.Item3, j)));
+                .CombineWithLatest(args.Item2.Observable, (i, j) => (i, j))
+                    .CombineWithLatest(args.Item3.Observable, (i, j) => (i.Item1, i.Item2, j))
+                        .CombineWithLatest(args.Item4.Observable, (i, j) => args.map(i.Item1, i.Item2, i.Item3, j)));
     }
-    public class CombineAllSinglesStreamNode<TIn1, TIn2, TIn3, TIn4, TIn5> : StreamNodeBase<Tuple<TIn1, TIn2, TIn3, TIn4, TIn5>, ISingleStream<Tuple<TIn1, TIn2, TIn3, TIn4, TIn5>>, Tuple<ISingleStream<TIn1>, ISingleStream<TIn2>, ISingleStream<TIn3>, ISingleStream<TIn4>, ISingleStream<TIn5>>>
+    public class CombineAllSinglesStreamNode<TIn1, TIn2, TIn3, TIn4, TIn5, TOut> : StreamNodeBase<TOut, ISingleStream<TOut>, (ISingleStream<TIn1>, ISingleStream<TIn2>, ISingleStream<TIn3>, ISingleStream<TIn4>, ISingleStream<TIn5>, Func<TIn1, TIn2, TIn3, TIn4, TIn5, TOut> map)>
     {
-        public CombineAllSinglesStreamNode(string name, Tuple<ISingleStream<TIn1>, ISingleStream<TIn2>, ISingleStream<TIn3>, ISingleStream<TIn4>, ISingleStream<TIn5>> args) : base(name, args) { }
+        public CombineAllSinglesStreamNode(string name, (ISingleStream<TIn1>, ISingleStream<TIn2>, ISingleStream<TIn3>, ISingleStream<TIn4>, ISingleStream<TIn5>, Func<TIn1, TIn2, TIn3, TIn4, TIn5, TOut> map) args) : base(name, args) { }
         public override ProcessImpact PerformanceImpact => ProcessImpact.Light;
         public override ProcessImpact MemoryFootPrint => ProcessImpact.Light;
-        protected override ISingleStream<Tuple<TIn1, TIn2, TIn3, TIn4, TIn5>> CreateOutputStream(Tuple<ISingleStream<TIn1>, ISingleStream<TIn2>, ISingleStream<TIn3>, ISingleStream<TIn4>, ISingleStream<TIn5>> args)
+        protected override ISingleStream<TOut> CreateOutputStream((ISingleStream<TIn1>, ISingleStream<TIn2>, ISingleStream<TIn3>, ISingleStream<TIn4>, ISingleStream<TIn5>, Func<TIn1, TIn2, TIn3, TIn4, TIn5, TOut> map) args)
             => base.CreateSingleStream(args.Item1.Observable
-                .CombineWithLatest(args.Item2.Observable, (i, j) => new Tuple<TIn1, TIn2>(i, j))
-                    .CombineWithLatest(args.Item3.Observable, (i, j) => new Tuple<TIn1, TIn2, TIn3>(i.Item1, i.Item2, j))
-                        .CombineWithLatest(args.Item4.Observable, (i, j) => new Tuple<TIn1, TIn2, TIn3, TIn4>(i.Item1, i.Item2, i.Item3, j))
-                            .CombineWithLatest(args.Item5.Observable, (i, j) => new Tuple<TIn1, TIn2, TIn3, TIn4, TIn5>(i.Item1, i.Item2, i.Item3, i.Item4, j)));
+                .CombineWithLatest(args.Item2.Observable, (i, j) => (i, j))
+                    .CombineWithLatest(args.Item3.Observable, (i, j) => (i.Item1, i.Item2, j))
+                        .CombineWithLatest(args.Item4.Observable, (i, j) => (i.Item1, i.Item2, i.Item3, j))
+                            .CombineWithLatest(args.Item5.Observable, (i, j) => args.map(i.Item1, i.Item2, i.Item3, i.Item4, j)));
     }
-    public class CombineAllSinglesStreamNode<TIn1, TIn2, TIn3, TIn4, TIn5, TIn6> : StreamNodeBase<Tuple<TIn1, TIn2, TIn3, TIn4, TIn5, TIn6>, ISingleStream<Tuple<TIn1, TIn2, TIn3, TIn4, TIn5, TIn6>>, Tuple<ISingleStream<TIn1>, ISingleStream<TIn2>, ISingleStream<TIn3>, ISingleStream<TIn4>, ISingleStream<TIn5>, ISingleStream<TIn6>>>
+    public class CombineAllSinglesStreamNode<TIn1, TIn2, TIn3, TIn4, TIn5, TIn6, TOut> : StreamNodeBase<TOut, ISingleStream<TOut>, (ISingleStream<TIn1>, ISingleStream<TIn2>, ISingleStream<TIn3>, ISingleStream<TIn4>, ISingleStream<TIn5>, ISingleStream<TIn6>, Func<TIn1, TIn2, TIn3, TIn4, TIn5, TIn6, TOut> map)>
     {
-        public CombineAllSinglesStreamNode(string name, Tuple<ISingleStream<TIn1>, ISingleStream<TIn2>, ISingleStream<TIn3>, ISingleStream<TIn4>, ISingleStream<TIn5>, ISingleStream<TIn6>> args) : base(name, args) { }
+        public CombineAllSinglesStreamNode(string name, (ISingleStream<TIn1>, ISingleStream<TIn2>, ISingleStream<TIn3>, ISingleStream<TIn4>, ISingleStream<TIn5>, ISingleStream<TIn6>, Func<TIn1, TIn2, TIn3, TIn4, TIn5, TIn6, TOut> map) args) : base(name, args) { }
         public override ProcessImpact PerformanceImpact => ProcessImpact.Light;
         public override ProcessImpact MemoryFootPrint => ProcessImpact.Light;
-        protected override ISingleStream<Tuple<TIn1, TIn2, TIn3, TIn4, TIn5, TIn6>> CreateOutputStream(Tuple<ISingleStream<TIn1>, ISingleStream<TIn2>, ISingleStream<TIn3>, ISingleStream<TIn4>, ISingleStream<TIn5>, ISingleStream<TIn6>> args)
+        protected override ISingleStream<TOut> CreateOutputStream((ISingleStream<TIn1>, ISingleStream<TIn2>, ISingleStream<TIn3>, ISingleStream<TIn4>, ISingleStream<TIn5>, ISingleStream<TIn6>, Func<TIn1, TIn2, TIn3, TIn4, TIn5, TIn6, TOut> map) args)
             => base.CreateSingleStream(args.Item1.Observable
-                .CombineWithLatest(args.Item2.Observable, (i, j) => new Tuple<TIn1, TIn2>(i, j))
-                    .CombineWithLatest(args.Item3.Observable, (i, j) => new Tuple<TIn1, TIn2, TIn3>(i.Item1, i.Item2, j))
-                        .CombineWithLatest(args.Item4.Observable, (i, j) => new Tuple<TIn1, TIn2, TIn3, TIn4>(i.Item1, i.Item2, i.Item3, j))
-                            .CombineWithLatest(args.Item5.Observable, (i, j) => new Tuple<TIn1, TIn2, TIn3, TIn4, TIn5>(i.Item1, i.Item2, i.Item3, i.Item4, j))
-                                .CombineWithLatest(args.Item6.Observable, (i, j) => new Tuple<TIn1, TIn2, TIn3, TIn4, TIn5, TIn6>(i.Item1, i.Item2, i.Item3, i.Item4, i.Item5, j)));
+                .CombineWithLatest(args.Item2.Observable, (i, j) => (i, j))
+                    .CombineWithLatest(args.Item3.Observable, (i, j) => (i.Item1, i.Item2, j))
+                        .CombineWithLatest(args.Item4.Observable, (i, j) => (i.Item1, i.Item2, i.Item3, j))
+                            .CombineWithLatest(args.Item5.Observable, (i, j) => (i.Item1, i.Item2, i.Item3, i.Item4, j))
+                                .CombineWithLatest(args.Item6.Observable, (i, j) => args.map(i.Item1, i.Item2, i.Item3, i.Item4, i.Item5, j)));
     }
-    public class CombineAllSinglesStreamNode<TIn1, TIn2, TIn3, TIn4, TIn5, TIn6, TIn7> : StreamNodeBase<Tuple<TIn1, TIn2, TIn3, TIn4, TIn5, TIn6, TIn7>, ISingleStream<Tuple<TIn1, TIn2, TIn3, TIn4, TIn5, TIn6, TIn7>>, Tuple<ISingleStream<TIn1>, ISingleStream<TIn2>, ISingleStream<TIn3>, ISingleStream<TIn4>, ISingleStream<TIn5>, ISingleStream<TIn6>, ISingleStream<TIn7>>>
+    public class CombineAllSinglesStreamNode<TIn1, TIn2, TIn3, TIn4, TIn5, TIn6, TIn7, TOut> : StreamNodeBase<TOut, ISingleStream<TOut>, (ISingleStream<TIn1>, ISingleStream<TIn2>, ISingleStream<TIn3>, ISingleStream<TIn4>, ISingleStream<TIn5>, ISingleStream<TIn6>, ISingleStream<TIn7>, Func<TIn1, TIn2, TIn3, TIn4, TIn5, TIn6, TIn7, TOut> map)>
     {
-        public CombineAllSinglesStreamNode(string name, Tuple<ISingleStream<TIn1>, ISingleStream<TIn2>, ISingleStream<TIn3>, ISingleStream<TIn4>, ISingleStream<TIn5>, ISingleStream<TIn6>, ISingleStream<TIn7>> args) : base(name, args) { }
+        public CombineAllSinglesStreamNode(string name, (ISingleStream<TIn1>, ISingleStream<TIn2>, ISingleStream<TIn3>, ISingleStream<TIn4>, ISingleStream<TIn5>, ISingleStream<TIn6>, ISingleStream<TIn7>, Func<TIn1, TIn2, TIn3, TIn4, TIn5, TIn6, TIn7, TOut> map) args) : base(name, args) { }
         public override ProcessImpact PerformanceImpact => ProcessImpact.Light;
         public override ProcessImpact MemoryFootPrint => ProcessImpact.Light;
-        protected override ISingleStream<Tuple<TIn1, TIn2, TIn3, TIn4, TIn5, TIn6, TIn7>> CreateOutputStream(Tuple<ISingleStream<TIn1>, ISingleStream<TIn2>, ISingleStream<TIn3>, ISingleStream<TIn4>, ISingleStream<TIn5>, ISingleStream<TIn6>, ISingleStream<TIn7>> args)
+        protected override ISingleStream<TOut> CreateOutputStream((ISingleStream<TIn1>, ISingleStream<TIn2>, ISingleStream<TIn3>, ISingleStream<TIn4>, ISingleStream<TIn5>, ISingleStream<TIn6>, ISingleStream<TIn7>, Func<TIn1, TIn2, TIn3, TIn4, TIn5, TIn6, TIn7, TOut> map) args)
             => base.CreateSingleStream(args.Item1.Observable
-                .CombineWithLatest(args.Item2.Observable, (i, j) => new Tuple<TIn1, TIn2>(i, j))
-                    .CombineWithLatest(args.Item3.Observable, (i, j) => new Tuple<TIn1, TIn2, TIn3>(i.Item1, i.Item2, j))
-                        .CombineWithLatest(args.Item4.Observable, (i, j) => new Tuple<TIn1, TIn2, TIn3, TIn4>(i.Item1, i.Item2, i.Item3, j))
-                            .CombineWithLatest(args.Item5.Observable, (i, j) => new Tuple<TIn1, TIn2, TIn3, TIn4, TIn5>(i.Item1, i.Item2, i.Item3, i.Item4, j))
-                                .CombineWithLatest(args.Item6.Observable, (i, j) => new Tuple<TIn1, TIn2, TIn3, TIn4, TIn5, TIn6>(i.Item1, i.Item2, i.Item3, i.Item4, i.Item5, j))
-                                    .CombineWithLatest(args.Item7.Observable, (i, j) => new Tuple<TIn1, TIn2, TIn3, TIn4, TIn5, TIn6, TIn7>(i.Item1, i.Item2, i.Item3, i.Item4, i.Item5, i.Item6, j)));
+                .CombineWithLatest(args.Item2.Observable, (i, j) => (i, j))
+                    .CombineWithLatest(args.Item3.Observable, (i, j) => (i.Item1, i.Item2, j))
+                        .CombineWithLatest(args.Item4.Observable, (i, j) => (i.Item1, i.Item2, i.Item3, j))
+                            .CombineWithLatest(args.Item5.Observable, (i, j) => (i.Item1, i.Item2, i.Item3, i.Item4, j))
+                                .CombineWithLatest(args.Item6.Observable, (i, j) => (i.Item1, i.Item2, i.Item3, i.Item4, i.Item5, j))
+                                    .CombineWithLatest(args.Item7.Observable, (i, j) => args.map(i.Item1, i.Item2, i.Item3, i.Item4, i.Item5, i.Item6, j)));
     }
 }
