ab07a2580f88fa92bafb48c6c556b0fd7fa58640
scixing <whs11998@163.com>
2022-09-20 23:38:46 +0800

    [Add] Excel, TextFile, Zip(Read) xUnitTests

---
+++ src/Paillave.Etl.XTest/ZipTests.cs
@@ -0,0 +1,45 @@
+﻿using Paillave.Etl.Core;
+using Paillave.Etl.FileSystem;
+using Paillave.Etl.TextFile;
+using Paillave.Etl.Zip;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Paillave.Etl.Testcases
+{
+    public class ZipTests
+    {
+        [Fact]
+        public static async Task ReadFromZipAsync()
+        {
+            var processRunner = Core.StreamProcessRunner.Create<string>(DefineProcessRead);
+            var res = await processRunner.ExecuteAsync("InputFiles");
+            Console.Write(res.Failed ? "Failed" : "Succeeded");
+        }
+        //[Fact]
+        public static async Task WriteToTextZipAsync()
+        {
+            throw new NotImplementedException();
+        }
+        private static void DefineProcessWrite(Core.ISingleStream<string> contextStream)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void DefineProcessRead(Core.ISingleStream<string> contextStream)
+        {
+            
+            contextStream.CrossApplyFolderFiles("get data", "*.zip")
+                .CrossApplyZipFiles("extract files from zip", "*.csv")
+                .CrossApplyTextFile("Parse data", FlatFileDefinition.Create(i => new
+                {
+                    a = i.ToColumn("a"),
+                    b = i.ToColumn("b")
+                }).IsColumnSeparated(','))
+                .Do("show data", i => Console.WriteLine($"a = {i.a}, b = {i.b}"));
+        }
+    }
+}
