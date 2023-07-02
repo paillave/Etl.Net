2d456a63e309cfdce85555e3b1f95f7464118834
Stephane Royer <stephane.royer@fundprocess.lu>
2023-07-02 21:04:32 +0200

    dotnet version for delete

--- src/Paillave.Etl.EntityFrameworkCore/EntityFrameworkCoreDelete.Stream.ex.cs
+++ src/Paillave.Etl.EntityFrameworkCore/EntityFrameworkCoreDelete.Stream.ex.cs
@@ -5,28 +5,6 @@ namespace Paillave.Etl.EntityFrameworkCore
 {
     public static class EntityFrameworkCoreDeleteEx
     {
-        // [Obsolete]
-        // public static IStream<TIn> EfCoreDelete<TIn, TEntity>(this IStream<TIn> inputStream, string name, Expression<Func<TIn, TEntity, bool>> match)
-        //     where TEntity : class
-        // {
-        //     return new DeleteEntityFrameworkCoreStreamNode<TIn, TIn, TEntity>(name, new DeleteEntityFrameworkCoreArgs<TIn, TIn, TEntity>
-        //     {
-        //         InputStream = inputStream,
-        //         Match = match,
-        //         GetValue = i => i
-        //     }).Output;
-        // }
-        // [Obsolete]
-        // public static IStream<Correlated<TIn>> EfCoreDelete<TIn, TEntity>(this IStream<Correlated<TIn>> inputStream, string name, Expression<Func<TIn, TEntity, bool>> match)
-        //     where TEntity : class
-        // {
-        //     return new DeleteEntityFrameworkCoreStreamNode<Correlated<TIn>, TIn, TEntity>(name, new DeleteEntityFrameworkCoreArgs<Correlated<TIn>, TIn, TEntity>
-        //     {
-        //         InputStream = inputStream,
-        //         Match = match,
-        //         GetValue = i => i.Row
-        //     }).Output;
-        // }
         public static IStream<TIn> EfCoreDelete<TIn, TEntity>(this IStream<TIn> inputStream, string name, Func<DeleteEntityFrameworkCoreArgsBuilder<TIn>, DeleteEntityFrameworkCoreArgsBuilder<TIn, TEntity>> builder)
             where TEntity : class
         {
