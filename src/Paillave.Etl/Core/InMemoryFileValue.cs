7dad8d92f882ba3e2b01079a9011766407ac0825
Stephane Royer <stephane.royer@fundprocess.lu>
2023-12-15 20:21:56 +0100

    bug reseek inmemoryfilevalue

--- src/Paillave.Etl/Core/InMemoryFileValue.cs
+++ src/Paillave.Etl/Core/InMemoryFileValue.cs
@@ -1,30 +1,30 @@
 using System.IO;
-namespace Paillave.Etl.Core
+namespace Paillave.Etl.Core;
+
+public class InMemoryFileValue<TMetadata> : FileValueBase<TMetadata> where TMetadata : IFileValueMetadata
 {
-    public class InMemoryFileValue<TMetadata> : FileValueBase<TMetadata> where TMetadata : IFileValueMetadata
+    private readonly Stream _stream;
+    public InMemoryFileValue(Stream stream, string name, TMetadata metadata)
+        : base(metadata) => (_stream, Name) = (stream, name);
+    public override string Name { get; }
+    public override Stream GetContent()
     {
-        private readonly Stream _stream;
-        public InMemoryFileValue(Stream stream, string name, TMetadata metadata)
-            : base(metadata) => (_stream, Name) = (stream, name);
-        public override string Name { get; }
-        public override Stream GetContent()
-        {
-            var ms = new MemoryStream();
-            _stream.CopyTo(ms);
-            ms.Seek(0, SeekOrigin.Begin);
-            return ms;
-        }
-
-        public override StreamWithResource OpenContent() => new StreamWithResource(_stream);
-
-        protected override void DeleteFile() { }
+        var ms = new MemoryStream();
+        _stream.Seek(0, SeekOrigin.Begin);
+        _stream.CopyTo(ms);
+        ms.Seek(0, SeekOrigin.Begin);
+        return ms;
     }
-    public static class FileValue
-    {
-        public static IFileValue Create<TMetadata>(Stream stream, string name, TMetadata metadata)
-            where TMetadata : IFileValueMetadata
-            => new InMemoryFileValue<TMetadata>(stream, name, metadata);
-        public static IFileValue Create(Stream stream, string name, string type)
-            => new InMemoryFileValue<NoSourceFileValueMetadata>(stream, name, new NoSourceFileValueMetadata(type));
-    }
+
+    public override StreamWithResource OpenContent() => new StreamWithResource(_stream);
+
+    protected override void DeleteFile() { }
+}
+public static class FileValue
+{
+    public static IFileValue Create<TMetadata>(Stream stream, string name, TMetadata metadata)
+        where TMetadata : IFileValueMetadata
+        => new InMemoryFileValue<TMetadata>(stream, name, metadata);
+    public static IFileValue Create(Stream stream, string name, string type)
+        => new InMemoryFileValue<NoSourceFileValueMetadata>(stream, name, new NoSourceFileValueMetadata(type));
 }
