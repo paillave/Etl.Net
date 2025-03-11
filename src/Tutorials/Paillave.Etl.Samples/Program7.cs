6529e6f140d85696efcdea63c2c8b00314ecad5b
CyrilGendarme <cyrilgendarme123@gmail.com>
2025-03-11 17:03:35 +0100

    Simple test program for HttpRest operator

--- src/Tutorials/Paillave.Etl.Samples/Program7.cs
+++ src/Tutorials/Paillave.Etl.Samples/Program7.cs
@@ -1,127 +1,127 @@
-﻿using System;
-using System.Threading.Tasks;
-using Paillave.Etl.Core;
-using Paillave.Etl.Http;
+﻿// using System;
+// using System.Threading.Tasks;
+// using Paillave.Etl.Core;
+// using Paillave.Etl.Http;
 
-namespace Paillave.Etl.Samples
-{
-    class Program7
-    {
-        static async Task Main(string[] args)
-        {
-            var processRunner = StreamProcessRunner.Create<string[]>(Import);
-            var httpClientFactory = new HttpClientFactory();
-            var executionOptions = new ExecutionOptions<string[]>
-            {
-                Resolver = new SimpleDependencyResolver().Register<HttpClientFactory>(
-                    httpClientFactory
-                ),
-            };
-            var res = await processRunner.ExecuteAsync(args, executionOptions);
-        }
+// namespace Paillave.Etl.Samples
+// {
+//     class Program7
+//     {
+//         static async Task Main(string[] args)
+//         {
+//             var processRunner = StreamProcessRunner.Create<string[]>(Import);
+//             var httpClientFactory = new HttpClientFactory();
+//             var executionOptions = new ExecutionOptions<string[]>
+//             {
+//                 Resolver = new SimpleDependencyResolver().Register<HttpClientFactory>(
+//                     httpClientFactory
+//                 ),
+//             };
+//             var res = await processRunner.ExecuteAsync(args, executionOptions);
+//         }
 
-        private static void DoOneTest(
-            ISingleStream<string[]> contextStream,
-            string testName,
-            string authType,
-            List<string> authParams,
-            string slug = "/headers",
-            string method = "Get",
-            string url = "http://127.0.0.1:80/"
-        )
-        {
-            var fileStream = contextStream
-                .CrossApply(
-                    "Get Files",
-                    new HttpFileValueProvider(
-                        code: "codeTest",
-                        url: url,
-                        method: method,
-                        slug: slug,
-                        responseFormat: "json",
-                        requestFormat: null, // Assuming requestFormat can be null
-                        authParameters: authParams,
-                        additionalHeaders: new List<KeyValuePair<string, string>>(),
-                        additionalParameters: new List<KeyValuePair<string, string>>(),
-                        AuthenticationType: authType,
-                        body: null
-                    )
-                )
-                .Do(
-                    "print to console",
-                    i =>
-                    {
-                        // Convert the Stream content into a byte array
-                        using (var memoryStream = new MemoryStream())
-                        {
-                            i.GetContent().CopyTo(memoryStream);
-                            byte[] contentBytes = memoryStream.ToArray();
-                            string contentString = System.Text.Encoding.UTF8.GetString(
-                                contentBytes
-                            ); // Convert byte array to string
-                            Console.WriteLine($"result ({testName}): {contentString}");
-                        }
-                    }
-                );
-        }
+//         private static void DoOneTest(
+//             ISingleStream<string[]> contextStream,
+//             string testName,
+//             string authType,
+//             List<string> authParams,
+//             string slug = "/headers",
+//             string method = "Get",
+//             string url = "http://127.0.0.1:80/"
+//         )
+//         {
+//             var fileStream = contextStream
+//                 .CrossApply(
+//                     "Get Files",
+//                     new HttpFileValueProvider(
+//                         code: "codeTest",
+//                         url: url,
+//                         method: method,
+//                         slug: slug,
+//                         responseFormat: "json",
+//                         requestFormat: null, // Assuming requestFormat can be null
+//                         authParameters: authParams,
+//                         additionalHeaders: new List<KeyValuePair<string, string>>(),
+//                         additionalParameters: new List<KeyValuePair<string, string>>(),
+//                         AuthenticationType: authType,
+//                         body: null
+//                     )
+//                 )
+//                 .Do(
+//                     "print to console",
+//                     i =>
+//                     {
+//                         // Convert the Stream content into a byte array
+//                         using (var memoryStream = new MemoryStream())
+//                         {
+//                             i.GetContent().CopyTo(memoryStream);
+//                             byte[] contentBytes = memoryStream.ToArray();
+//                             string contentString = System.Text.Encoding.UTF8.GetString(
+//                                 contentBytes
+//                             ); // Convert byte array to string
+//                             Console.WriteLine($"result ({testName}): {contentString}");
+//                         }
+//                     }
+//                 );
+//         }
 
-        public static void Import(ISingleStream<string[]> contextStream)
-        {
-            // DoOneTest(contextStream, "no auth", "None", new List<string>());
+//         public static void Import(ISingleStream<string[]> contextStream)
+//         {
+//             // DoOneTest(contextStream, "no auth", "None", new List<string>());
 
-            // DoOneTest(
-            //     contextStream,
-            //     "Bearer auth",
-            //     "Bearer",
-            //     new List<string>() { "AbCdEf123456" },
-            //     "/bearer"
-            // );
+//             // DoOneTest(
+//             //     contextStream,
+//             //     "Bearer auth",
+//             //     "Bearer",
+//             //     new List<string>() { "AbCdEf123456" },
+//             //     "/bearer"
+//             // );
 
-            // DoOneTest(
-            //     contextStream,
-            //     "Basic auth (OK)",
-            //     "Basic",
-            //     new List<string>() { "usr", "pwd" },
-            //     "/basic-auth/usr/pwd"
-            // );
+//             // DoOneTest(
+//             //     contextStream,
+//             //     "Basic auth (OK)",
+//             //     "Basic",
+//             //     new List<string>() { "usr", "pwd" },
+//             //     "/basic-auth/usr/pwd"
+//             // );
 
-            DoOneTest(
-                contextStream,
-                "Basic auth (NOK)",
-                "Basic",
-                new List<string>() { "usr", "pwdWrong" },
-                "/basic-auth/usr/pwd"
-            );
+//             DoOneTest(
+//                 contextStream,
+//                 "Basic auth (NOK)",
+//                 "Basic",
+//                 new List<string>() { "usr", "pwdWrong" },
+//                 "/basic-auth/usr/pwd"
+//             );
 
-            // DoOneTest(
-            //     contextStream,
-            //     "Digest (usr/pwd endpoint)",
-            //     "Digest",
-            //     new List<string>() { "usr", "pwd" },
-            //     "/digest-auth/auth/usr/pwd"
-            // );
+//             // DoOneTest(
+//             //     contextStream,
+//             //     "Digest (usr/pwd endpoint)",
+//             //     "Digest",
+//             //     new List<string>() { "usr", "pwd" },
+//             //     "/digest-auth/auth/usr/pwd"
+//             // );
 
-            // DoOneTest(
-            //     contextStream,
-            //     "Digest (usr/pwd/MD5 endpoint)",
-            //     "Digest",
-            //     new List<string>() { "usr", "pwd", "auth", "MD5" },
-            //     "/digest-auth/auth/usr/pwd/MD5"
-            // );
+//             // DoOneTest(
+//             //     contextStream,
+//             //     "Digest (usr/pwd/MD5 endpoint)",
+//             //     "Digest",
+//             //     new List<string>() { "usr", "pwd", "auth", "MD5" },
+//             //     "/digest-auth/auth/usr/pwd/MD5"
+//             // );
 
-            // DoOneTest(
-            //     contextStream,
-            //     "ApiKey auth",
-            //     "ApiKey",
-            //     new List<string>() { "ApiKeyExemple" }
-            // );
+//             // DoOneTest(
+//             //     contextStream,
+//             //     "ApiKey auth",
+//             //     "ApiKey",
+//             //     new List<string>() { "ApiKeyExemple" }
+//             // );
 
-            // DoOneTest(
-            //     contextStream,
-            //     "CustomHeader auth",
-            //     "CustomHeader",
-            //     new List<string>() { "arg1", "val1", "arg2", "val2" }
-            // );
-        }
-    }
-}
+//             // DoOneTest(
+//             //     contextStream,
+//             //     "CustomHeader auth",
+//             //     "CustomHeader",
+//             //     new List<string>() { "arg1", "val1", "arg2", "val2" }
+//             // );
+//         }
+//     }
+// }
---
+++ src/Tutorials/Paillave.Etl.Samples/Program9.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Threading.Tasks;
+using Paillave.Etl.Core;
+using Paillave.Etl.Http;
+
+namespace Paillave.Etl.Samples
+{
+    class Program9
+    {
+        static async Task Main(string[] args)
+        {
+            var processRunner = StreamProcessRunner.Create<string[]>(Import);
+            var httpClientFactory = new HttpClientFactory();
+            var executionOptions = new ExecutionOptions<string[]>
+            {
+                Resolver = new SimpleDependencyResolver().Register<HttpClientFactory>(
+                    httpClientFactory
+                ),
+            };
+            var res = await processRunner.ExecuteAsync(args, executionOptions);
+        }
+
+        private static void DoOneTest(
+            ISingleStream<string[]> contextStream,
+            string testName,
+            string authType,
+            List<string> authParams,
+            string slug = "/headers",
+            string method = "Get",
+            string url = "http://127.0.0.1:80/"
+        )
+        {
+            var fileStream = contextStream
+                .CrossApply(
+                    "Get Files",
+                    new HttpFileValueProvider(
+                        code: "codeTest",
+                        url: url,
+                        method: method,
+                        slug: slug,
+                        responseFormat: "json",
+                        requestFormat: null, // Assuming requestFormat can be null
+                        authParameters: authParams,
+                        additionalHeaders: new List<KeyValuePair<string, string>>(),
+                        additionalParameters: new List<KeyValuePair<string, string>>(),
+                        AuthenticationType: authType,
+                        body: null
+                    )
+                )
+                .Do(
+                    "print to console",
+                    i =>
+                    {
+                        // Convert the Stream content into a byte array
+                        using (var memoryStream = new MemoryStream())
+                        {
+                            i.GetContent().CopyTo(memoryStream);
+                            byte[] contentBytes = memoryStream.ToArray();
+                            string contentString = System.Text.Encoding.UTF8.GetString(
+                                contentBytes
+                            ); // Convert byte array to string
+                            Console.WriteLine($"result ({testName}): {contentString}");
+                        }
+                    }
+                );
+        }
+
+        public static void Import(ISingleStream<string[]> contextStream)
+        {
+            contextStream
+                .Select(
+                    "create required stream item type",
+                    i => new HttpCallArgs
+                    {
+                        ConnectionParameters = new HttpAdapterConnectionParameters
+                        {
+                            Url = "http://127.0.0.1:80/",
+                        },
+                        AdapterParameters = new HttpAdapterParametersBase
+                        {
+                            Method = "Get",
+                            Slug = "/ip",
+                        },
+                    }
+                )
+                .HttpRest("http rest operator test")
+                .Do(
+                    "print to console",
+                    i =>
+                    {
+                        using (var memoryStream = new MemoryStream())
+                        {
+                            string contentString = System.Text.Encoding.UTF8.GetString(
+                                i.Content.ReadAsByteArrayAsync().Result
+                            );
+                            Console.WriteLine($"result : {contentString}");
+                        }
+                    }
+                );
+        }
+    }
+}
