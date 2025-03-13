73a8b89ed27a73624f15c2bd2a139ef17ad87d08
CyrilGendarme <cyrilgendarme123@gmail.com>
2025-03-13 11:37:00 +0100

    all auth logic in HttpAuthentication.cs

---
+++ src/Paillave.Etl.Http/HttpAuthentication.cs
@@ -0,0 +1,96 @@
+using System;
+using System.Linq.Expressions;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace Paillave.Etl.Http;
+
+public class HttpAuthentication
+{
+    public BearerAuthentication? Bearer { get; set; }
+    public BasicAuthentication? Basic { get; set; }
+    public DigestAuthentication? Digest { get; set; }
+}
+
+public abstract class AbstractAuthentication {
+    public abstract HttpClient AddAuthenticationHeaders(HttpClient client);
+}
+
+public class DigestAuthentication : AbstractAuthentication
+{
+    public required string Url { get; set; }
+    public required string User { get; set; }
+    public required string Password { get; set; }
+    public required string QualityOfProtection { get; set; }
+    public required DigestAlgorithm Algorithm { get; set; } = DigestAlgorithm.MD5;
+
+    public override HttpClient AddAuthenticationHeaders(HttpClient client){
+        throw new NotImplementedException("Digest authentication not tested enough");
+
+        var response = client.GetAsync(Url).Result;
+
+        // If response is 401, extract necessary values (like nonce) from the headers
+        if (response.StatusCode == System.Net.HttpStatusCode.Unauthorized)
+        {
+            var authHeader = response.Headers.WwwAuthenticate.ToString();
+            string realm = Helpers.ExtractHeaderValue(authHeader, "realm");
+            string nonce = Helpers.ExtractHeaderValue(authHeader, "nonce");
+            string opaque = Helpers.ExtractHeaderValue(authHeader, "opaque");
+
+            // Step 2: Generate the Digest Authentication header
+            var digestAuthHeader = Helpers.GenerateDigestAuthHeader(
+                User,
+                Password,
+                realm,
+                nonce,
+                QualityOfProtection,
+                Url,
+                opaque
+            );
+
+            // Step 3: Send the request again with the Digest Authentication header
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue(
+                "Digest",
+                digestAuthHeader
+            );
+        }
+
+        return client;
+    }
+}
+
+public enum DigestAlgorithm
+{
+    MD5 = 0,
+}
+
+public class BasicAuthentication : AbstractAuthentication
+{
+    public required string User { get; set; }
+    public required string Password { get; set; }
+
+    public override HttpClient AddAuthenticationHeaders(HttpClient client){
+        string credentials = $"{User}:{Password}";
+        string base64Credentials = Convert.ToBase64String(
+            Encoding.UTF8.GetBytes(credentials)
+        );
+        client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue(
+            "Basic",
+            base64Credentials
+        );
+        return client;
+    }
+}
+
+public class BearerAuthentication : AbstractAuthentication
+{
+    public required string Authority { get; set; }
+    public required string ClientId { get; set; }
+    public required string Passphrase { get; set; }
+    
+    public override HttpClient AddAuthenticationHeaders(HttpClient client){
+        throw new NotImplementedException("Bearer authentication not implemented");
+        // return client;
+    }
+}
