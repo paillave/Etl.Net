7a4853138a1fa49319736a14e63c86548928b2ba
Stephane Royer <stephane.royer@fundprocess.lu>
2025-11-18 12:11:34 +0100

    Refactor MultiTenantDbContext to improve tenant handling and update version to 2.3.22-beta.0

--- src/Paillave.EntityFrameworkCoreExtension/Core/MultiTenantDbContext.cs
+++ src/Paillave.EntityFrameworkCoreExtension/Core/MultiTenantDbContext.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using System.Linq;
 using Microsoft.EntityFrameworkCore.Migrations;
 using System.Threading.Tasks;
@@ -7,68 +6,40 @@ using System.Threading;
 using System.Collections.Generic;
 using Paillave.EntityFrameworkCoreExtension.MigrationOperations;
 using System.Collections;
-using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.ChangeTracking;
 
 namespace Paillave.EntityFrameworkCoreExtension.Core;
 
-public class MultiTenantDbContext : DbContext
+public interface ITenantProvider
 {
-    private object _sync = new object();
-    private readonly string? _collation;
-
-    public MultiTenantDbContext(int tenantId)
-    {
-        this.TenantId = tenantId;
-
-    }
-    public int TenantId { get; }
-    private Dictionary<Type, IEntityType>? _inTenantEntities = null;
-    // private record EntitySummary(IProperty TenantIdProperty)
-    private Dictionary<Type, IEntityType> InTenantEntities => _inTenantEntities ??= this.Model.GetEntityTypes().Where(i => i.FindProperty("TenantId") != null).ToDictionary(i => i.ClrType);
-
-    // https://docs.microsoft.com/en-us/ef/core/providers/sqlite/limitations
-    // https://docs.microsoft.com/en-us/ef/core/managing-schemas/migrations/providers?tabs=dotnet-core-cli
-
-    public MultiTenantDbContext(DbContextOptions options, int tenantId, string? collation = null) : base(options)
-    {
-        this.TenantId = tenantId;
-        this._collation = collation;
-    }
+    int Current { get; }
+}
+public class MultiTenantDbContext(DbContextOptions options, ITenantProvider? tenantProvider = null) : DbContext(options)
+{
+    private readonly Lock _sync = new();
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        // For EFCore5
-        if (!string.IsNullOrWhiteSpace(_collation))
-        {
-            modelBuilder.UseCollation(_collation);
-        }
-        // https://github.com/aspnet/EntityFrameworkCore/blob/a9c6cb3548df771a57af97f0aafe55009464f8f9/src/EFCore/ModelBuilder.cs#L266
-        ApplyConfigurationsFromAssembly(modelBuilder);
+        modelBuilder.ApplyConfigurationsFromAssembly(this.GetType().Assembly);
+        SetupMultiTenant(modelBuilder);
     }
-    private void ApplyConfigurationsFromAssembly(ModelBuilder modelBuilder)
+    private void SetupMultiTenant(ModelBuilder modelBuilder)
     {
-        var applyEntityConfigurationMethod = typeof(ModelBuilder)
-            .GetMethods()
-            .Single(e =>
-                e.Name == nameof(ModelBuilder.ApplyConfiguration)
-                && e.ContainsGenericParameters
-                && e.GetParameters().SingleOrDefault()?.ParameterType.GetGenericTypeDefinition() == typeof(IEntityTypeConfiguration<>));
+        if (tenantProvider == null) return;
 
-        foreach (var type in this.GetType().Assembly.GetConstructibleTypes())
+        foreach (var entityType in modelBuilder.Model.GetEntityTypes().Where(et => et.FindProperty("TenantId") != null))
         {
-            foreach (var @interface in type.GetInterfaces().Where(i => i.IsGenericType && i.GetGenericTypeDefinition() == typeof(IEntityTypeConfiguration<>)))
-            {
-                var target = applyEntityConfigurationMethod.MakeGenericMethod(@interface.GenericTypeArguments[0]);
-                if (type.GetConstructor(Type.EmptyTypes) != null)
-                {
-                    target.Invoke(modelBuilder, [Activator.CreateInstance(type)]);
-                }
-                else if (type.GetConstructor([this.GetType()]) != null)
-                {
-                    target.Invoke(modelBuilder, [Activator.CreateInstance(type, [this])]);
-                }
-            }
+            var method = this.GetType()
+                .GetMethod(nameof(SetupMultiTenant), System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance)!
+                .MakeGenericMethod(entityType.ClrType);
+            method.Invoke(this, [modelBuilder]);
         }
+    }
+
+    private void SetupMultiTenant<TEntity>(ModelBuilder modelBuilder) where TEntity : class
+    {
+        if (tenantProvider == null) return;
+        modelBuilder.Entity<TEntity>().Property<int>("TenantId").IsRequired();
+        modelBuilder.Entity<TEntity>().HasQueryFilter(i => EF.Property<int>(i, "TenantId") == tenantProvider.Current);
     }
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
@@ -123,8 +94,9 @@ public class MultiTenantDbContext : DbContext
     {
         if (processedEntities.Contains(entityEntry.Entity)) return;
         processedEntities.Add(entityEntry.Entity);
-        if (TenantId == 0 || !InTenantEntities.TryGetValue(entityEntry.Metadata.ClrType, out var entityType)) return;
-        entityEntry.Property("TenantId").CurrentValue = TenantId;
+        var tenantId = tenantProvider?.Current;
+        if ((tenantId ?? 0) == 0) return;
+        entityEntry.Property("TenantId").CurrentValue = tenantId;
         foreach (var navigation in entityEntry.Navigations)
         {
             if (navigation.CurrentValue != null)
