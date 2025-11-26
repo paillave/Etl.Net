8ccc42a68a0e652dd619a5aefe471095715eccde
Stephane Royer <stephane.royer@fundprocess.lu>
2025-11-26 14:28:58 +0100

    Remove obsolete sample import files and update program structure
    - Deleted TestImport2.cs and TestImport3.cs as they are no longer needed.
    - Removed old versions of Program.cs, Program3.cs, Program4.cs, Program6.cs, Program7.cs, Program8.cs, and Program9.cs to clean up the sample directory.
    - Ensured that the remaining code adheres to the latest standards and practices.

--- src/Tutorials/Paillave.Etl.Samples/DataAccess/TestDbContext.cs
+++ src/Tutorials/Paillave.Etl.Samples/DataAccess/TestDbContext.cs
@@ -1,16 +1,11 @@
 using Microsoft.EntityFrameworkCore;
 
-namespace Paillave.Etl.Samples.DataAccess
+namespace Paillave.Etl.Samples.DataAccess;
+
+public class TestDbContext(DbContextOptions options) : DbContext(options)
 {
-    public class TestDbContext : DbContext
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder.UseSqlServer(@"Server=localhost,1433;Initial Catalog=TestEtl;Persist Security Info=False;User ID=SA;Password=<YourStrong@Passw0rd>;MultipleActiveResultSets=True;Encrypt=True;TrustServerCertificate=true;Connection Timeout=300;");
-        }
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            modelBuilder.ApplyConfigurationsFromAssembly(this.GetType().Assembly);
-        }
+        modelBuilder.ApplyConfigurationsFromAssembly(this.GetType().Assembly);
     }
 }
--- src/Tutorials/Paillave.Etl.Samples/Program2.cs
+++ src/Tutorials/Paillave.Etl.Samples/Program2.cs
@@ -1,162 +1,171 @@
-﻿// // using System;
-// // // using Paillave.Etl.FromConfigurationConnectors;
-// // using Paillave.Etl.FileSystem;
-// // using Paillave.Etl.Core;
-// // using Paillave.Etl.ExecutionToolkit;
-// // using System.Threading.Tasks;
-// // using System.IO;
+﻿// using System;
 // // using Paillave.Etl.FromConfigurationConnectors;
 // using Paillave.Etl.FileSystem;
 // using Paillave.Etl.Core;
 // using Paillave.Etl.ExecutionToolkit;
+// using System.Threading.Tasks;
+// using System.IO;
 // using Paillave.Etl.FromConfigurationConnectors;
-// using Paillave.Etl.Ftp;
-// using Paillave.Etl.Sftp;
-// using Paillave.Etl.Mail;
-// using Paillave.Etl.Pgp;
+using Paillave.Etl.FileSystem;
+using Paillave.Etl.Core;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
 
-// namespace Paillave.Etl.Samples
-// {
-//     class Program
-//     {
-//         static async Task Main(string[] args)
-//         {
-//             CreateConnectorConfigurationFileSchema();
-//             // await ConnectorTestAsync(args);
-//             // await ImportAndCreateFileAsync(args);
-//             // await ImportAndCreateFileWithConfigAsync(args);
-//         }
-//         private static ConfigurationFileValueConnectorParser CreateConfigurationFileValueConnectorParser() => new ConfigurationFileValueConnectorParser(
-//                 new PgpProviderProcessorAdapter(),
-//                 new FileSystemProviderProcessorAdapter(),
-//                 new FtpProviderProcessorAdapter(),
-//                 new SftpProviderProcessorAdapter(),
-//                 new MailProviderProcessorAdapter()
-//                 );
-//         public static void CreateConnectorConfigurationFileSchema()
-//             => File.WriteAllText(Path.Combine(Environment.CurrentDirectory, "connectorsConfigSchema.json"), CreateConfigurationFileValueConnectorParser().GetConnectorsSchemaJson());
+namespace Paillave.Etl.Samples;
 
-// //         /// <summary>
-// //         /// Example 1: simple importation of a set of files
-// //         /// </summary>
-// //         /// <param name="args"></param>
-// //         /// <returns></returns>
-// //         static async Task SimplyImportAsync(string[] args)
-// //         {
-// //             var processRunner = StreamProcessRunner.Create<string[]>(TestImport.Import);
-// //             // processRunner.GetDefinitionStructure().OpenEstimatedExecutionPlan();
-// //             var output = await processRunner.ExecuteAsync(args, new ExecutionOptions<string[]>
-// //             {
-// //                 Connectors = new FileValueConnectors()
-// //                     .Register(new FileSystemFileValueProvider("PTF", "Portfolios", Path.Combine(Environment.CurrentDirectory, "InputFiles"), "*.Portfolios.csv"))
-// //                     .Register(new FileSystemFileValueProvider("POS", "Positions", Path.Combine(Environment.CurrentDirectory, "InputFiles"), "*.Positions.csv")),
-// //                 Resolver = new SimpleDependencyResolver()
-// //                     .Register(new DataAccess.TestDbContext()),
-// //             });
-// //         }
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        await SimplyImportAsync(args);
+        // CreateConnectorConfigurationFileSchema();
+        // await ConnectorTestAsync(args);
+        // await ImportAndCreateFileAsync(args);
+        // await ImportAndCreateFileWithConfigAsync(args);
+    }
+    // private static ConfigurationFileValueConnectorParser CreateConfigurationFileValueConnectorParser() => new(
+    //     new FileSystemProviderProcessorAdapter());
+    // public static void CreateConnectorConfigurationFileSchema()
+    //     => File.WriteAllText(Path.Combine(Environment.CurrentDirectory, "connectorsConfigSchema.json"), CreateConfigurationFileValueConnectorParser().GetConnectorsSchemaJson());
 
-// //         /// <summary>
-// //         /// Example 2: Import and export files showing the estimated execution plan, the real time evolution of the process, and the actual execution plan
-// //         /// </summary>
-// //         /// <param name="args"></param>
-// //         /// <returns></returns>
-// //         static async Task ImportAndCreateFileAsync(string[] args)
-// //         {
-// //             var processRunner = StreamProcessRunner.Create<string[]>(TestImport2.Import);
-// //             var structure = processRunner.GetDefinitionStructure();
-// //             // structure.OpenEstimatedExecutionPlan();
+    //         /// <summary>
+    //         /// Example 1: simple importation of a set of files
+    //         /// </summary>
+    //         /// <param name="args"></param>
+    //         /// <returns></returns>
+    static async Task SimplyImportAsync(string[] args)
+    {
+        var services = new ServiceCollection()
+            .AddPooledDbContextFactory<DataAccess.TestDbContext>(options => options.UseSqlServer(@"Server=localhost,1433;Initial Catalog=TestEtl;Persist Security Info=False;User ID=SA;Password=<YourStrong@Passw0rd>;MultipleActiveResultSets=True;Encrypt=True;TrustServerCertificate=true;Connection Timeout=300;", options => options
+                .CommandTimeout(2000)
+                .UseQuerySplittingBehavior(QuerySplittingBehavior.SplitQuery)))
+            .AddTransient<DbContext>(sp => sp
+                .GetRequiredService<IDbContextFactory<DataAccess.TestDbContext>>().CreateDbContext())
+            .AddSingleton<IFileValueConnectors>(new FileValueConnectors()
+                .Register(new FileSystemFileValueProvider("PTF", "Portfolios", Path.Combine(Environment.CurrentDirectory, "InputFiles"), "*.Portfolios.csv"))
+                .Register(new FileSystemFileValueProvider("POS", "Positions", Path.Combine(Environment.CurrentDirectory, "InputFiles"), "*.Positions.csv")))
+            .BuildServiceProvider();
 
-// //             // ITraceReporter traceReporter = new AdvancedConsoleExecutionDisplay();
-// //             ITraceReporter traceReporter = new SimpleConsoleExecutionDisplay();
-// //             var dataAccess = new DataAccess.TestDbContext();
-// //             await dataAccess.Database.EnsureCreatedAsync();
-// //             // dataAccess.Database.Migrate();
-// //             var executionOptions = new ExecutionOptions<string[]>
-// //             {
-// //                 Connectors = new FileValueConnectors()
-// //                     .Register(new FileSystemFileValueProvider("PTF", "Portfolios", Path.Combine(Environment.CurrentDirectory, "InputFiles"), "*.Portfolios.csv"))
-// //                     .Register(new FileSystemFileValueProvider("POS", "Positions", Path.Combine(Environment.CurrentDirectory, "InputFiles"), "*.Positions.csv"))
-// //                     .Register(new FileSystemFileValueProcessor("OUT", "Result", Path.Combine(Environment.CurrentDirectory, "InputFiles"))),
-// //                 Resolver = new SimpleDependencyResolver()
-// //                     .Register(dataAccess),
-// //                 TraceProcessDefinition = traceReporter.TraceProcessDefinition,
-// //             };
-// //             traceReporter.Initialize(structure);
+        using (var dbCtx = services.GetRequiredService<DbContext>())
+            dbCtx.Database.Migrate();
 
-//             var res = await processRunner.ExecuteAsync(args, executionOptions);
-//             res.OpenActualExecutionPlan();
-//         }
-//         static async Task ConnectorTestAsync(string[] args)
-//         {
-//             var processRunner = StreamProcessRunner.Create<string[]>(i => i
-//                 .FromConnector("in", "IN")
-//                 .Do("write on console",context =>
-//                 {
-//                     Console.WriteLine($"Processing {context.Name}");
-//                 })
-//                 // .ToConnector("out", "OUT")
-//             );
-//             var structure = processRunner.GetDefinitionStructure();
-//             // structure.OpenEstimatedExecutionPlan();
+        var executionOptions = new ExecutionOptions<string[]>
+        {
+            Services = services,
+            TraceProcessDefinition = (ts, cs) => ts.Do("Show trace on console", t => Console.WriteLine(t.ToString())),
+            // TraceProcessDefinition = DefineTraceProcess,
+            // UseDetailedTraces = true // activate only if per row traces are meant to be caught
+        };
 
-//             ITraceReporter traceReporter = new SimpleConsoleExecutionDisplay();
-//             // var dataAccess = new DataAccess.TestDbContext();
-//             // await dataAccess.Database.EnsureCreatedAsync();
-//             // dataAccess.Database.Migrate();
-//             var executionOptions = new ExecutionOptions<string[]>
-//             {
-//                 Connectors = new FileValueConnectors()
-//                     .Register(
-//                         new SftpFileValueProvider("IN", "Input", "Sftp", new SftpAdapterConnectionParameters
-//                         {
-//                             Server = "localhost",
-//                             PortNumber = 22,
-//                             Login = ConsoleAsk("login:"),
-//                             Password = ConsoleAsk("password:"),
-//                             MaxAttempts = 3,
-//                             RootFolder = "stephane",
-//                             // RootFolder = "/stephane/Documents",
-//                         },
-//                         new SftpAdapterProviderParameters
-//                         {
-//                             SubFolder = "Documents",
-//                             // SubFolder = "",
-//                             FileNamePattern = "*"
-//                         }))
-//                     .Register(new FileSystemFileValueProcessor("OUT", "Output", Path.Combine(Environment.CurrentDirectory, "InputFiles"))),
-//                 // Resolver = new SimpleDependencyResolver()
-//                 //     .Register(dataAccess),
-//                 TraceProcessDefinition = traceReporter.TraceProcessDefinition,
-//             };
-//             traceReporter.Initialize(structure);
 
-//             var res = await processRunner.ExecuteAsync(args, executionOptions);
-//             // res.OpenActualExecutionPlan();
-//         }
-//         static string? ConsoleAsk(string question)
-//         {
-//             Console.WriteLine(question);
-//             return Console.ReadLine();
-//         }
 
-// //         /// <summary>
-// //         /// Example 3: Import and export files using a config file to setup connectors
-// //         /// </summary>
-// //         /// <param name="args"></param>
-// //         /// <returns></returns>
-// //         static async Task ImportAndCreateFileWithConfigAsync(string[] args)
-// //         {
-// //             var processRunner = StreamProcessRunner.Create<string[]>(TestImport2.Import);
-// //             var dataAccess = new DataAccess.TestDbContext();
-// //             await dataAccess.Database.EnsureCreatedAsync();
-// //             var executionOptions = new ExecutionOptions<string[]>
-// //             {
-// //                 Connectors = CreateConfigurationFileValueConnectorParser()
-// //                     .GetConnectors(File.ReadAllText(Path.Combine(Environment.CurrentDirectory, "connectorsLocalConfig.json"))),
-// //                 Resolver = new SimpleDependencyResolver()
-// //                     .Register(dataAccess)
-// //             };
-// //             var res = await processRunner.ExecuteAsync(args, executionOptions);
-// //         }
-// //     }
-// // }
+        var processRunner = StreamProcessRunner.Create<string[]>(TestImport.Import);
+        // processRunner.GetDefinitionStructure().OpenEstimatedExecutionPlan();
+        var output = await processRunner.ExecuteAsync(args, executionOptions);
+    }
+
+    //         /// <summary>
+    //         /// Example 2: Import and export files showing the estimated execution plan, the real time evolution of the process, and the actual execution plan
+    //         /// </summary>
+    //         /// <param name="args"></param>
+    //         /// <returns></returns>
+    //         static async Task ImportAndCreateFileAsync(string[] args)
+    //         {
+    //             var processRunner = StreamProcessRunner.Create<string[]>(TestImport2.Import);
+    //             var structure = processRunner.GetDefinitionStructure();
+    //             // structure.OpenEstimatedExecutionPlan();
+
+    //             // ITraceReporter traceReporter = new AdvancedConsoleExecutionDisplay();
+    //             ITraceReporter traceReporter = new SimpleConsoleExecutionDisplay();
+    //             var dataAccess = new DataAccess.TestDbContext();
+    //             await dataAccess.Database.EnsureCreatedAsync();
+    //             // dataAccess.Database.Migrate();
+    //             var executionOptions = new ExecutionOptions<string[]>
+    //             {
+    //                 Connectors = new FileValueConnectors()
+    //                     .Register(new FileSystemFileValueProvider("PTF", "Portfolios", Path.Combine(Environment.CurrentDirectory, "InputFiles"), "*.Portfolios.csv"))
+    //                     .Register(new FileSystemFileValueProvider("POS", "Positions", Path.Combine(Environment.CurrentDirectory, "InputFiles"), "*.Positions.csv"))
+    //                     .Register(new FileSystemFileValueProcessor("OUT", "Result", Path.Combine(Environment.CurrentDirectory, "InputFiles"))),
+    //                 Resolver = new SimpleDependencyResolver()
+    //                     .Register(dataAccess),
+    //                 TraceProcessDefinition = traceReporter.TraceProcessDefinition,
+    //             };
+    //             traceReporter.Initialize(structure);
+
+    // var res = await processRunner.ExecuteAsync(args, executionOptions);
+    // res.OpenActualExecutionPlan();
+    // }
+    // static async Task ConnectorTestAsync(string[] args)
+    // {
+    //     var processRunner = StreamProcessRunner.Create<string[]>(i => i
+    //         .FromConnector("in", "IN")
+    //         .Do("write on console", context =>
+    //         {
+    //             Console.WriteLine($"Processing {context.Name}");
+    //         })
+    //     // .ToConnector("out", "OUT")
+    //     );
+    //     var structure = processRunner.GetDefinitionStructure();
+    //     // structure.OpenEstimatedExecutionPlan();
+
+    //     ITraceReporter traceReporter = new SimpleConsoleExecutionDisplay();
+    //     // var dataAccess = new DataAccess.TestDbContext();
+    //     // await dataAccess.Database.EnsureCreatedAsync();
+    //     // dataAccess.Database.Migrate();
+    //     var executionOptions = new ExecutionOptions<string[]>
+    //     {
+    //         Connectors = new FileValueConnectors()
+    //             .Register(
+    //                 new SftpFileValueProvider("IN", "Input", "Sftp", new SftpAdapterConnectionParameters
+    //                 {
+    //                     Server = "localhost",
+    //                     PortNumber = 22,
+    //                     Login = ConsoleAsk("login:"),
+    //                     Password = ConsoleAsk("password:"),
+    //                     MaxAttempts = 3,
+    //                     RootFolder = "stephane",
+    //                     // RootFolder = "/stephane/Documents",
+    //                 },
+    //                 new SftpAdapterProviderParameters
+    //                 {
+    //                     SubFolder = "Documents",
+    //                     // SubFolder = "",
+    //                     FileNamePattern = "*"
+    //                 }))
+    //             .Register(new FileSystemFileValueProcessor("OUT", "Output", Path.Combine(Environment.CurrentDirectory, "InputFiles"))),
+    //         // Resolver = new SimpleDependencyResolver()
+    //         //     .Register(dataAccess),
+    //         TraceProcessDefinition = traceReporter.TraceProcessDefinition,
+    //     };
+    //     traceReporter.Initialize(structure);
+
+    //     var res = await processRunner.ExecuteAsync(args, executionOptions);
+    //     // res.OpenActualExecutionPlan();
+    // }
+    // static string? ConsoleAsk(string question)
+    // {
+    //     Console.WriteLine(question);
+    //     return Console.ReadLine();
+    // }
+
+    //         /// <summary>
+    //         /// Example 3: Import and export files using a config file to setup connectors
+    //         /// </summary>
+    //         /// <param name="args"></param>
+    //         /// <returns></returns>
+    //         static async Task ImportAndCreateFileWithConfigAsync(string[] args)
+    //         {
+    //             var processRunner = StreamProcessRunner.Create<string[]>(TestImport2.Import);
+    //             var dataAccess = new DataAccess.TestDbContext();
+    //             await dataAccess.Database.EnsureCreatedAsync();
+    //             var executionOptions = new ExecutionOptions<string[]>
+    //             {
+    //                 Connectors = CreateConfigurationFileValueConnectorParser()
+    //                     .GetConnectors(File.ReadAllText(Path.Combine(Environment.CurrentDirectory, "connectorsLocalConfig.json"))),
+    //                 Resolver = new SimpleDependencyResolver()
+    //                     .Register(dataAccess)
+    //             };
+    //             var res = await processRunner.ExecuteAsync(args, executionOptions);
+    //         }
+    //     }
+}
