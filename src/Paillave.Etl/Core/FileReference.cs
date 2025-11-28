cf8967efcae0fc5a91b4fbd2cc62365cd097818a
Stéphane Royer <stephane.royer@fundprocess.lu>
2025-11-28 15:05:52 +0100

    feat: Update metadata handling to use JsonNode; increment version to 2.3.35-beta.0

--- src/Paillave.Etl/Core/FileReference.cs
+++ src/Paillave.Etl/Core/FileReference.cs
@@ -1,23 +1,23 @@
 using System.Collections.Generic;
 using System.IO;
+using System.Text.Json.Nodes;
 
 namespace Paillave.Etl.Core;
 
 public class FileReference(string name, string connector, string fileSpecific)
 {
-    public string FileSpecific { get; } = fileSpecific;
-    public string Name { get; } = name;
-    public string Connector { get; } = connector;
-
+    public string FileSpecific => fileSpecific;
+    public string Name => name;
+    public string Connector => connector;
     public Stream GetContent(IFileValueConnectors connectors)
         => connectors.GetProvider(Connector).Provide(FileSpecific).GetContent();
     public void Delete(IFileValueConnectors connectors)
         => connectors.GetProvider(Connector).Provide(FileSpecific).Delete();
-    public void SendToConnector(IFileValueConnectors connectors, string targetConnector, Dictionary<string, IEnumerable<Destination>>? destinations = null, object? metadata = null)
+    public void SendToConnector(IFileValueConnectors connectors, string targetConnector, Dictionary<string, IEnumerable<Destination>>? destinations = null, JsonNode? targetMetadata = null)
     {
         var fileValue = connectors.GetProvider(Connector).Provide(FileSpecific);
         fileValue.Destinations = destinations;
-        fileValue.Metadata = metadata;
+        fileValue.Metadata = targetMetadata;
         connectors.GetProcessor(targetConnector).Process(fileValue, _ => { }, default);
     }
 }
